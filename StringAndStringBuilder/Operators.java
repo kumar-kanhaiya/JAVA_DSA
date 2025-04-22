7843921ee84444fb0a007ddc918a230d6e30a69f
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-04-22 17:48:23 +0530

    Add files via upload

--- StringAndStringBuilder/Operators.java
+++ StringAndStringBuilder/Operators.java
@@ -3,5 +3,9 @@ package StringAndStringBuilder;
 public class Operators {
     public static void main(String[] args) {
         System.out.println('a' + 'b'); // return ASCI value of a + ASCI value of b
+        System.out.println("a" + "b"); // it return a+b ;
+        System.out.println((char)('a' + 3)); // it print d
+        System.out.println("a" + 1); // integer will be converted to Integer that will call to string
+        
     }
 }
