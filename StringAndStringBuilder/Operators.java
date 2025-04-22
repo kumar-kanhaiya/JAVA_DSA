f58a416fed11bdfda79498a2ec1cfddfac6e18c8
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-04-22 11:45:33 +0530

    Add files via upload

---
+++ StringAndStringBuilder/Operators.java
@@ -0,0 +1,7 @@
+package StringAndStringBuilder;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b'); // return ASCI value of a + ASCI value of b
+    }
+}
