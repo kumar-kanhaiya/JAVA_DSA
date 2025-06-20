12dd625638b430bc10a28a8c59469e85158b86c9
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-20 10:41:13 +0530

    code

--- src/Recursion/fibonacciSeriesUsingGoldenFormula.java
+++ src/Recursion/fibonacciSeriesUsingGoldenFormula.java
@@ -1,6 +1,8 @@
 package Recursion;
 // finding fibonacci series by using golden formula
-
+// also known as golden ratio
+//formula
+// 1+ root 5 by 2 == time complexibility bego( 1.6180)^n
 public class fibonacciSeriesUsingGoldenFormula {
     public static void main(String[] args) {
 
