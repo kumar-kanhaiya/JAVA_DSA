c353b0f26103d3b492a13a6d282015193e0dbf89
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-06-20 10:43:44 +0530

    code

--- src/Recursion/fibonacciSeriesUsingGoldenFormula.java
+++ src/Recursion/fibonacciSeriesUsingGoldenFormula.java
@@ -7,4 +7,7 @@ public class fibonacciSeriesUsingGoldenFormula {
     public static void main(String[] args) {
 
     }
+    static int fiboFormula(int n){
+       return (int)Math.pow(((1+ Math.sqrt(5)) / 2) , n) ;
+    }
 }
