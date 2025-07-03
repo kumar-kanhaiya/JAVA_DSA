f488193b31d7aede21a8d1be22f2cd0ab0993a58
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-03 12:29:31 +0530

    code

--- src/Recursion/fibonacciSeriesUsingGoldenFormula.java
+++ src/Recursion/fibonacciSeriesUsingGoldenFormula.java
@@ -12,6 +12,7 @@ public class fibonacciSeriesUsingGoldenFormula {
     }
     static int fiboFormula(int n){
         // just for demo use long as int
+
         return (int)((Math.pow(((1+ Math.sqrt(5)) / 2) , n)  / Math.sqrt(5)));
 //        Math.pow(((1 - Math.sqrt(5)) / 2) , n)) you can remove this because it is less dominating term
     }
