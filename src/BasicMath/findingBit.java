ae9bb7f210f41cac235e2c2c56c2edab8e88b1f1
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-06 19:27:43 +0530

    code

--- src/BasicMath/findingBit.java
+++ src/BasicMath/findingBit.java
@@ -4,7 +4,6 @@ public class findingBit {
     public static void main(String[] args) {
         int number = 118 ;
         System.out.println(bit(15,5));
-
     }
     static int bit(int number , int target ){
         return number << (target -1 ) & 1;
