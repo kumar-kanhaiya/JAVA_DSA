d913a1e9a810a33811b215e722edcb51219c80af
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-06 20:13:51 +0530

    code

--- src/BasicMath/Question_7.java
+++ src/BasicMath/Question_7.java
@@ -6,6 +6,11 @@ public class Question_7 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int number ;
+        System.out.print("Enter number : ");
+        number = input.nextInt();
+        System.out.println(ans(number));
+    }
+    public static int ans(int number ){
 
     }
 }
