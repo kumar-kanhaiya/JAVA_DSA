2f811243f8b98062a0e1da7e8a2a8c8328622e8f
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-06 20:12:31 +0530

    code

--- src/BasicMath/Question_7.java
+++ src/BasicMath/Question_7.java
@@ -1,7 +1,11 @@
 package BasicMath;
 
+import java.util.Scanner;
+
 public class Question_7 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number ;
 
     }
 }
