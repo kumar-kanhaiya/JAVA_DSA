d2594898d8be85baeaf2ed8a26055edd9bb60113
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-07 13:24:09 +0530

    code

--- src/BasicMath/Power.java
+++ src/BasicMath/Power.java
@@ -2,6 +2,12 @@ package BasicMath;
 
 public class Power {
     public static void main(String[] args) {
+        int base = 3 ;
+        int power = 6 ;
+        int ans = 1;
 
+        while(power > 0){
+
+        }
     }
 }
