3015af48b072a97cdda7c9b2dd66241eba9632c6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-07 12:12:43 +0530

    code

--- src/BasicMath/MagicNumber.java
+++ src/BasicMath/MagicNumber.java
@@ -5,10 +5,12 @@ public class MagicNumber {
     // find nth magic number :
         int n = 6 ;
         int ans = 0;
+        int base = 5;
         while( n  > 0){
             ans = n & 1 ;
             n = n >> 1 ;
+            base = base * 5 ;
         }
-
+        System.out.println(ans);
     }
 }
