577ddc44411383ca09388abd3de98b472d87b33e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-07 13:46:26 +0530

    code

--- src/BasicMath/numberOfSetBit.java
+++ src/BasicMath/numberOfSetBit.java
@@ -14,14 +14,14 @@ public class numberOfSetBit {
         System.out.println(count);
 
         // second approach
-        System.out.println(setBits(number));
+        System.out.println(setBits(197));
     }
 
     public static int setBits(int n) {
         int count = 0;
-             while (n >0){
-                 count++;
-                n -= (n & -n);
+        while(n > 0){
+            count++;
+            n = n & (n-1);
         }
 
         return count;
