138716d047a37f37930c337be117a83e600fb7ac
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-07 13:30:30 +0530

    code

--- src/BasicMath/numberOfSetBit.java
+++ src/BasicMath/numberOfSetBit.java
@@ -2,6 +2,14 @@ package BasicMath;
 
 public class numberOfSetBit {
     public static void main(String[] args) {
-
+    // normal approach
+        int number = 6 ;
+        int count = 0 ;
+        while(number > 0){
+            if((number & 1) == 1){
+                count++;
+            }
+            number = number>>1;
+        }
     }
 }
