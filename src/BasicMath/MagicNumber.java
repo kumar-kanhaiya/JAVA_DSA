0bfb1c6b8abd9fc97fbbc1365106ba1b4721b9aa
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-07 12:10:22 +0530

    code

--- src/BasicMath/MagicNumber.java
+++ src/BasicMath/MagicNumber.java
@@ -3,6 +3,10 @@ package BasicMath;
 public class MagicNumber {
     public static void main(String[] args) {
     // find nth magic number :
+        int n = 6 ;
+        int ans = 0;
+        while( n  > 0){
 
+        }
     }
 }
