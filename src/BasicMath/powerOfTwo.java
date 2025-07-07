adef4106ce58077be692e4efce96a671e47c90b8
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-07 13:12:48 +0530

    code

--- src/BasicMath/powerOfTwo.java
+++ src/BasicMath/powerOfTwo.java
@@ -2,7 +2,7 @@ package BasicMath;
 
 public class powerOfTwo {
     public static void main(String[] args) {
-        int n = 4 ;
+        int n = 16 ;
         boolean ans = (n & (n-1)) == 0 ;
         System.out.println(ans);
     }
