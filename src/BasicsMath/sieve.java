f680641bddbbe867fd0f77c789f2b7d6a138ff1d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 11:36:27 +0530

    code

--- src/BasicsMath/sieve.java
+++ src/BasicsMath/sieve.java
@@ -2,6 +2,23 @@ package BasicsMath;
 
 public class sieve {
     public static void main(String[] args) {
-
+        int n = 40;
+        boolean[] prime = new boolean[n+1];
+        seives(n , prime);
+    }
+    // initially all value is false
+    public static void seives(int n , boolean[] prime){
+        for (int i = 2; i*i <= n ; i++) {
+            if(!prime[i]){
+                for (int j = i*2; j <= n ; j+=i) {
+                    prime[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if(!prime[i]){
+                System.out.print(i + " ");
+            }
+        }
     }
 }
