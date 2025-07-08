2add8add7a64305d4cdf9d11bbb934ad4e3ded7d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 11:09:31 +0530

    code

--- src/BasicsMath/PrintAllPrimeNo.java
+++ src/BasicsMath/PrintAllPrimeNo.java
@@ -1,7 +1,23 @@
 package BasicsMath;
 
+import java.util.Scanner;
+
 public class PrintAllPrimeNo {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+    }
+    public static boolean isPrime(int number ){
+        if(number <= 1){
+            return false;
+        }
+        int c = 2;
+        while(c*c <= number){
+            if(number%c == 0){
+                return false;
+            }
+            c++;
+        }
+        return true;
     }
 }
