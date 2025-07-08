2eed75163e2f6213152d0a1dd19b29b2b7198508
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 11:55:57 +0530

    code

--- src/BasicsMath/squareRoot.java
+++ src/BasicsMath/squareRoot.java
@@ -2,6 +2,27 @@ package BasicsMath;
 
 public class squareRoot {
     public static void main(String[] args) {
+    int number = 36;
+        System.out.println(squareroot(number));
 
     }
+    public static int squareroot(int number){
+        int start = 0 ;
+        int end = number ;
+        while(end >= start){
+            int mid = start + (end - start)/2 ;
+            if(mid*mid == number){
+                // ans found
+                return mid;
+            }
+            else if(mid*mid > number){
+                end = mid - 1;
+            }
+            else{
+                start = mid +1 ;
+            }
+        }
+        return -1;
+    }
+
 }
