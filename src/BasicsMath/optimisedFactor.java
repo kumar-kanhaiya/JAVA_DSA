daf804934a2bb1417ec65860f720d3d0c99e257e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 20:03:25 +0530

    code

--- src/BasicsMath/optimisedFactor.java
+++ src/BasicsMath/optimisedFactor.java
@@ -2,6 +2,19 @@ package BasicsMath;
 
 public class optimisedFactor {
     public static void main(String[] args) {
+        factor(20);
+    }
+    public static void factor(int number ){
+        for (int i = 1; i <= Math.sqrt(number) ; i++) {
+            if(number%i == 0 ){
+                if(number/i == i){
+                    System.out.print(i);
+                }
+                else{
 
+                System.out.print( i + " " + (number/i) + " ");
+                }
+            }
+        }
     }
 }
