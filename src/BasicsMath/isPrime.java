9c0ad9f5a011551683ef6736349a6c33a04adc84
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 11:01:25 +0530

    code

--- src/BasicsMath/isPrime.java
+++ src/BasicsMath/isPrime.java
@@ -7,14 +7,17 @@ public class isPrime {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter number : ");
         int number = input.nextInt();
-        System.out.println(isprime(number));
+//        System.out.println(isprime(number));
+        for (int i = 0; i <= number ; i++) {
+            System.out.println(i + " " + isprime(i));
+        }
     }
     static boolean isprime(int number){
         if(number <= 1){
             return false;
         }
 //        boolean ans = true;
-        for (int i = 2; i*i < number ; i++) {
+        for (int i = 2; i*i <= number ; i++) {
             if(number%i == 0 ){
                 return false;
             }
