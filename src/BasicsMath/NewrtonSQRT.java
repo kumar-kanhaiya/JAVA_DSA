8dde6142c759a0529db53ea758387c75571af5c7
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 19:37:43 +0530

    code

--- src/BasicsMath/NewrtonSQRT.java
+++ src/BasicsMath/NewrtonSQRT.java
@@ -2,6 +2,19 @@ package BasicsMath;
 
 public class NewrtonSQRT {
     public static void main(String[] args) {
+        System.out.println(sqrt(40));
+    }
+    public static double sqrt(double n ){
+        double x = n;
+        double root;
+        while(true){
+            root = 0.5 * (x + (n/x));
 
+            if(Math.abs(root - x) < 1){
+                break;
+            }
+            x = root;
+        }
+        return root;
     }
 }
