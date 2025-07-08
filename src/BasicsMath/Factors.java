bf2f8af0036cb6a752bf955707aa858b2c555b00
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 19:43:24 +0530

    code

--- src/BasicsMath/Factors.java
+++ src/BasicsMath/Factors.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Factors {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int number;
+        System.out.print("Enter your number : ");
+        number = input.nextInt();
+        factor(number);
 
     }
     public static void factor(int number ){
