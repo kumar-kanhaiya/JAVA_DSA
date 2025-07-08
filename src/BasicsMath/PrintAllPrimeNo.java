5770f3d42df36a243fd40b94299dc839e0db58aa
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 11:11:58 +0530

    code git push

--- src/BasicsMath/PrintAllPrimeNo.java
+++ src/BasicsMath/PrintAllPrimeNo.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class PrintAllPrimeNo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter your number ");
+        int number = input.nextInt();
+        System.out.println("All prime number between 0 & " + number);
+        for (int i = 0; i < number ; i++) {
+            boolean check = isPrime(i);
+            if(check == false){
+                continue;
+            }
+            System.out.println(i);
+        }
 
     }
     public static boolean isPrime(int number ){
