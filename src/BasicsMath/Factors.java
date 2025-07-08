7009605e2f672ac513b43012f85bd53428aacf6a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 19:42:26 +0530

    code

--- src/BasicsMath/Factors.java
+++ src/BasicsMath/Factors.java
@@ -1,8 +1,17 @@
 package BasicsMath;
 
+import java.util.Scanner;
+
 public class Factors {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
     }
-
+    public static void factor(int number ){
+        for (int i = 1; i <= number ; i++) {
+            if(number%i == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
