feb243ed554fe1924def72189827878b5bbdced6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 20:10:41 +0530

    code

---
+++ src/BasicsMath/modulos.java
@@ -0,0 +1,13 @@
+package BasicsMath;
+
+public class modulos {
+    public static void main(String[] args) {
+        /* properties of modulos
+        (a+b)%m  = ((a % m) + (b % m)) % m
+        (a - b) % m  = ((a % m) - (b % m)) % m
+        (a * b)%m  = ((a % m) * (b % m)) % m
+        (a / b)%m  = ((a % m) * (b inverse  % m)) % m
+        b inverse means multiplicative modulos inverse
+         */
+    }
+}
