26dc36cbeb36abb651af3f91ecc52928f3517e3e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-08 10:14:16 +0530

    code

--- src/BasicsMath/isPrime.java
+++ src/BasicsMath/isPrime.java
@@ -1,7 +1,11 @@
 package BasicsMath;
 
+import java.util.Scanner;
+
 public class isPrime {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int number = input.nextInt();
     }
 }
