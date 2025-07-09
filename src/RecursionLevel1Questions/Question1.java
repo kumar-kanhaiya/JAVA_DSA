454b47ce77f290190bc6af45c66211babafa16ed
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-09 20:21:32 +0530

    code

--- src/RecursionLevel1Questions/Question1.java
+++ src/RecursionLevel1Questions/Question1.java
@@ -1,8 +1,24 @@
 package RecursionLevel1Questions;
 
+import java.util.Scanner;
+
 public class Question1 {
     public static void main(String[] args) {
-
+    // printing n to 1 number using recursion
+        //  simple recursive call
+        Scanner input = new Scanner(System.in);
+        int number ;
+        System.out.print("Enter your number : " );
+        number = input.nextInt();
+        System.out.println(print(number));
+    }
+    public static int print(int number ){
+        // Base Case :
+        if(number == 1){
+            return 1;
+        }
+        System.out.print(number + " ");
+        return print(number - 1);
     }
 
 }
