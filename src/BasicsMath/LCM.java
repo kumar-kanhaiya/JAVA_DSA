27769f92e67a2906106c6c24168299828cedd1f0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-09 14:06:52 +0530

    code

--- src/BasicsMath/LCM.java
+++ src/BasicsMath/LCM.java
@@ -2,10 +2,15 @@ package BasicsMath;
 
 public class LCM {
     public static void main(String[] args) {
-
+        System.out.println(lcm(5,6));
     }
-    // first and easy approach
+    public static int hcf (int a , int b ){
+        if(a == 0){
+            return b;
+        }
+        return hcf(b%a,a);
+    }
     public static int lcm(int a , int b ){
-
+        return (a*b)/hcf(a,b);
     }
 }
