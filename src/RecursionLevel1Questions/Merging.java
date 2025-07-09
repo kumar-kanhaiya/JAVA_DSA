1371f71706717cff6f065eb1d99bb8666a5c2c72
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-09 20:47:34 +0530

    code

--- src/RecursionLevel1Questions/Merging.java
+++ src/RecursionLevel1Questions/Merging.java
@@ -2,6 +2,14 @@ package RecursionLevel1Questions;
 
 public class Merging {
     public static void main(String[] args) {
-
+    merge(5);
+    }
+    public static void merge(int number ){
+        if(number == 0 ){
+            return;
+        }
+        System.out.println(number);
+        merge(number-1);
+        System.out.println(number);
     }
 }
