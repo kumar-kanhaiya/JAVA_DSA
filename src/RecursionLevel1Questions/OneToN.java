0a7572a5a3be88c29a3561f42c68a4bcd6aad8ed
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-09 20:30:15 +0530

    code

--- src/RecursionLevel1Questions/OneToN.java
+++ src/RecursionLevel1Questions/OneToN.java
@@ -4,4 +4,8 @@ public class OneToN {
     public static void main(String[] args) {
 
     }
+    // this function is used to print number from 1 to N
+    public static int Printing(int number ){
+
+    }
 }
