dc9dcf58df216225c1748d384ddaa1f0d03743c6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-09 12:43:06 +0530

    code

--- src/BasicsMath/HCF.java
+++ src/BasicsMath/HCF.java
@@ -2,12 +2,13 @@ package BasicsMath;
 
 public class HCF {
     public static void main(String[] args) {
+        System.out.println(hcf(4,18));
 
     }
     public static int hcf(int first , int secong ){
         int min = min(first , secong);
         int ans = 1;
-        for (int i = 1; i < min ; i++) {
+        for (int i = 1; i <= min ; i++) {
             if(first%i == 0 && secong%i == 0){
                 ans = i;
             }
