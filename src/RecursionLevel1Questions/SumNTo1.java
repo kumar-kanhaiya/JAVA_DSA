384adcdf3500c576a3908997f0e7aeceffff4dc3
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-09 21:01:37 +0530

    code

--- src/RecursionLevel1Questions/SumNTo1.java
+++ src/RecursionLevel1Questions/SumNTo1.java
@@ -2,7 +2,7 @@ package RecursionLevel1Questions;
 
 public class SumNTo1 {
     public static void main(String[] args) {
-
+        System.out.println(sum(5));
     }
     public static int sum(int number ){
         if(number == 1 ){
