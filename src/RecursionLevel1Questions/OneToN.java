ff488e298d371dea41d9a06df76e596665f0bc46
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-09 20:36:16 +0530

    code

--- src/RecursionLevel1Questions/OneToN.java
+++ src/RecursionLevel1Questions/OneToN.java
@@ -2,16 +2,16 @@ package RecursionLevel1Questions;
 
 public class OneToN {
     public static void main(String[] args) {
+        System.out.println(Printing(12, 1));
 
     }
     // this function is used to print number from 1 to N
-    public static int Printing(int number ){
-        int n = 1;
+    public static int Printing(int number , int n ){
         if(n == number){
             return number;
         }
         System.out.print(n + " ");
-        n++;
-        return Printing(number -1 );
+
+        return Printing(number  , n+1 );
     }
 }
