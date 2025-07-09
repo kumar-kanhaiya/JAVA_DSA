d1022941df90b54eee6a223f80191001af8dd38b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-09 20:43:37 +0530

    code

--- src/RecursionLevel1Questions/OneToN.java
+++ src/RecursionLevel1Questions/OneToN.java
@@ -4,6 +4,8 @@ public class OneToN {
     public static void main(String[] args) {
         System.out.println(Printing(12, 1));
 
+        printRev(12);
+
     }
     // this function is used to print number from 1 to N
     public static int Printing(int number , int n ){
@@ -13,5 +15,13 @@ public class OneToN {
         System.out.print(n + " ");
 
         return Printing(number  , n+1 );
+    }
+    // second Approach
+    public static void printRev(int number ){
+        if(number == 0){
+            return;
+        }
+        printRev(number - 1);
+        System.out.print(number + " ");
     }
 }
