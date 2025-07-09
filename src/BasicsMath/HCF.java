128321177e13ea34ba63e8fccf63c93dee890f86
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-09 12:41:18 +0530

    code

--- src/BasicsMath/HCF.java
+++ src/BasicsMath/HCF.java
@@ -4,4 +4,19 @@ public class HCF {
     public static void main(String[] args) {
 
     }
+    public static int hcf(int first , int secong ){
+        int min = min(first , secong);
+        int ans = 1;
+        for (int i = 1; i < min ; i++) {
+            if(first%i == 0 && secong%i == 0){
+
+            }
+        }
+    }
+    public static int min(int first , int second ){
+        if(first < second){
+            return first;
+        }
+        return second;
+    }
 }
