b2f0f8a4a3bf1bd2ca8b04418de8bcf6119e9762
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-09 21:01:13 +0530

    code

--- src/RecursionLevel1Questions/SumNTo1.java
+++ src/RecursionLevel1Questions/SumNTo1.java
@@ -4,4 +4,10 @@ public class SumNTo1 {
     public static void main(String[] args) {
 
     }
+    public static int sum(int number ){
+        if(number == 1 ){
+            return 1;
+        }
+        return number + sum(number - 1);
+    }
 }
