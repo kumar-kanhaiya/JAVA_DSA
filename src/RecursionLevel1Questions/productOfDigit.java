4173f2a6894ba9f99ed224defe1c6717616e622a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-10 08:58:44 +0530

    code

--- src/RecursionLevel1Questions/productOfDigit.java
+++ src/RecursionLevel1Questions/productOfDigit.java
@@ -2,6 +2,12 @@ package RecursionLevel1Questions;
 
 public class productOfDigit {
     public static void main(String[] args) {
-
+        System.out.println(ProductOfDigit(25));
+    }
+    public static int ProductOfDigit(int n ){
+        if(n < 1 ){
+            return 1;
+        }
+        return (n%10) * ProductOfDigit(n/10);
     }
 }
