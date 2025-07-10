fd9fe0566a612e6be012fe0385f34cf4ce4cd2ad
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-10 21:21:26 +0530

    code

--- src/RecursionLevel1Questions/reverse.java
+++ src/RecursionLevel1Questions/reverse.java
@@ -2,6 +2,18 @@ package RecursionLevel1Questions;
 
 public class reverse {
     public static void main(String[] args) {
+    reverseNumber(123);
+    }
+    public static void reverseNumber(int number ){
+        if(number < 1 ){
+            return ;
+        }
+//        return (number%10) * 10 + reverseNumber(number/10);
+        int rem = (number%10);
+        int ans = 0;
+        ans = ans*10 +  rem ;
+        reverseNumber(number/10);
+        System.out.print(ans);
 
     }
 }
