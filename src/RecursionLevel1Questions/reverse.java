63ee991cddff2d783a90fed6f68835c03460196b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-10 22:12:26 +0530

    code

--- src/RecursionLevel1Questions/reverse.java
+++ src/RecursionLevel1Questions/reverse.java
@@ -2,8 +2,7 @@ package RecursionLevel1Questions;
 
 public class reverse {
     public static void main(String[] args) {
-    reverseNumber(123);
-        System.out.println(sum);
+        System.out.println(rev2(54321));
 
     }
     // first approach
@@ -16,7 +15,20 @@ public class reverse {
         sum = sum*10 +rem;
         reverseNumber(number/10);
     }
+    // second approach
+    static int rev2(int n){
+        int digit = (int)(Math.log10(n)) + 1;
+        return helper(n , digit);
 
+    }
+
+    private static int helper(int n, int digit) {
+        if(n%10 == n){
+            return n;
+        }
+        int rem = n%10;
+        return rem * (int) Math.pow(10,digit-1) + helper(n/10 , digit-1);
+    }
 
 
 }
