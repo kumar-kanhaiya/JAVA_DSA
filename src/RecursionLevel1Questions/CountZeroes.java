8ab84a6bb52275668afba7d1f0736f649f5357b8
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-11 12:22:30 +0530

    code

--- src/RecursionLevel1Questions/CountZeroes.java
+++ src/RecursionLevel1Questions/CountZeroes.java
@@ -4,6 +4,7 @@ public class CountZeroes {
     public static void main(String[] args) {
         counting(10056);
         System.out.println(count);
+        System.out.println(count(10056));
     }
     // using recursion
     static int count = 0;
@@ -18,5 +19,18 @@ public class CountZeroes {
     }
 
     // second Way
+    // this program is done without using any global variable
+    static int count(int number){
+        return halper(number , 0);
+    }
 
+    private static int halper(int number, int i) {
+        if(number < 1){
+            return i;
+        }
+        if(number%10 == 0){
+            halper(number/10 , i++);
+        }
+        return halper(number/10 , i);
+    }
 }
