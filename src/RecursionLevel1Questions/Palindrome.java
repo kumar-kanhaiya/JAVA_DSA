1a8464a276b45579c3c73078a24d1b8fb2fb42c0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-11 11:34:53 +0530

    code

--- src/RecursionLevel1Questions/Palindrome.java
+++ src/RecursionLevel1Questions/Palindrome.java
@@ -2,7 +2,22 @@ package RecursionLevel1Questions;
 
 public class Palindrome {
     public static void main(String[] args) {
-
+    isPalindrome(525);
     }
     // normal approach without recursion
+    public static void isPalindrome(int number ){
+        int real = number;
+        int reverse = 0;
+        while(number > 0){
+            int rem = number%10;
+            reverse = reverse*10 + rem;
+            number/=10;
+        }
+        if(real == reverse){
+            System.out.println("This is palindrome ");
+        }
+        else{
+            System.out.println("this is not palindrome ");
+        }
+    }
 }
