defc5bcc0cfc2cfc8bcaea998629e06709620ca0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-11 12:07:50 +0530

    code

--- src/RecursionLevel1Questions/CountZeroes.java
+++ src/RecursionLevel1Questions/CountZeroes.java
@@ -2,7 +2,16 @@ package RecursionLevel1Questions;
 
 public class CountZeroes {
     public static void main(String[] args) {
-
     }
     // using recursion
+    static int count = 0;
+    public static void counting(int number ){
+        if(number< 1 ){
+            return;
+        }
+        if(number%10 == 0){
+            count++;
+        }
+        counting(number/10);
+    }
 }
