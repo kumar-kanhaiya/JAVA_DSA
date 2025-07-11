23b641acfa42312517a249153cefb25ab606bed4
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-11 12:39:56 +0530

    code

--- src/RecursionLevel1Questions/Leetcode1342.java
+++ src/RecursionLevel1Questions/Leetcode1342.java
@@ -2,6 +2,19 @@ package RecursionLevel1Questions;
 
 public class Leetcode1342 {
     public static void main(String[] args) {
+        System.out.println(numberOfSteps(14));
+    }
+    public static int numberOfSteps(int num) {
+        return helper(num , 0);
+    }
 
+    public static int helper(int num, int i) {
+        if(num < 1){
+            return i;
+        }
+        if(num%2 == 0){
+            return helper(num/2 , i = i +1 );
+        }
+        return helper(num - 1 , i= i +1);
     }
 }
