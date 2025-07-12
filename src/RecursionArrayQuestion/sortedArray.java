34f1415ce85acb36256e74e9fd41b204cc4db9f2
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-12 10:12:55 +0530

    code

--- src/RecursionArrayQuestion/sortedArray.java
+++ src/RecursionArrayQuestion/sortedArray.java
@@ -2,6 +2,22 @@ package RecursionArrayQuestion;
 
 public class sortedArray {
     public static void main(String[] args) {
+    // find if array is sorted or not
+        // arr = {1,2,4,8,9,12}
+        int[] arr = { 1,2,4,8,9,12};
+        System.out.println(isSorted(arr));
 
+    }
+    public static boolean isSorted(int[] array ){
+        return helper(array , 0);
+    }
+    static boolean helper(int[] array , int start ){
+        if(start == array.length - 1){
+            return true;
+        }
+        if(array[start] > array[start + 1]){
+            return false;
+        }
+        return helper(array , start + 1);
     }
 }
