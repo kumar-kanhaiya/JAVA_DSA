d961fcf2c9ff4195de21d47fbb647b9687738f23
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-12 11:54:06 +0530

    code

--- src/RecursionArrayQuestion/ReturningArrayList.java
+++ src/RecursionArrayQuestion/ReturningArrayList.java
@@ -1,7 +1,20 @@
 package RecursionArrayQuestion;
 
+import java.util.ArrayList;
+
 public class ReturningArrayList {
     public static void main(String[] args) {
+        System.out.println(check());
 
+    }
+    // first approach by passing argument
+    public static ArrayList<Integer> check(int[] array , int target , int start , ArrayList<Integer> list){
+        if(start == array.length){
+            return list;
+        }
+        if(array[start] == target){
+            list.add(start);
+        }
+        return check(array , target , start + 1 , list);
     }
 }
