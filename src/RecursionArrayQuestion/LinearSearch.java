8fc6546d30cd5aa39d35ed2b5e658f4c3106eccc
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-12 10:52:19 +0530

    code

--- src/RecursionArrayQuestion/LinearSearch.java
+++ src/RecursionArrayQuestion/LinearSearch.java
@@ -3,5 +3,17 @@ package RecursionArrayQuestion;
 public class LinearSearch {
     public static void main(String[] args) {
 // find out the indexing of target element in the array using recursion
+        int[] array
+    }
+    public static int check(int[] array , int target , int start){
+        if(start > array.length - 1){
+            // ans not found
+            return -1;
+        }
+        // ans found
+        if(array[start] == target){
+            return start;
+        }
+        return check(array,target , start + 1);
     }
 }
