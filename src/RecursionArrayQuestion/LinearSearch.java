272440f3caf8976fba72f52d7d4e9022804bc6b7
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-12 10:38:21 +0530

    code

--- src/RecursionArrayQuestion/LinearSearch.java
+++ src/RecursionArrayQuestion/LinearSearch.java
@@ -2,6 +2,6 @@ package RecursionArrayQuestion;
 
 public class LinearSearch {
     public static void main(String[] args) {
-
+// find out the indexing of target element in the array using recursion
     }
 }
