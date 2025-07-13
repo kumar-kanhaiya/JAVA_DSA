68cbee32a2dbad2c3afaac22f68ff90f2489a4f6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-13 15:06:00 +0530

    code

--- src/RecursionArrayQuestion/RoatatedBinarySearch.java
+++ src/RecursionArrayQuestion/RoatatedBinarySearch.java
@@ -5,24 +5,8 @@ import java.util.Arrays;
 public class RoatatedBinarySearch {
     public static void main(String[] args) {
         int[] array = {5,6,7,8,9,1,2,3};
-        rotation(array);
-        System.out.println(Arrays.toString(array));
+
 
     }
-    public static void rotation(int[] array ){
-        int start = 0 ;
-        int end = array.length - 1;
 
-            for (int i = 0; i <array.length  ; i++) {
-                if(array[i] < array[array.length - 1 - i]){
-                    swap(array,i,end - i );
-                }
-            }
-        }
-
-    public static void swap(int[] array , int first , int second){
-        int temp = array[first];
-        array[first] = array[second];
-        array[second] =temp;
-    }
 }
