80435a65d691f1b442f2fa2fe763e9a80c42b750
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-13 15:52:02 +0530

    code

--- src/RecursionPatternQuestions/Triangle.java
+++ src/RecursionPatternQuestions/Triangle.java
@@ -2,7 +2,18 @@ package RecursionPatternQuestions;
 
 public class Triangle {
     public static void main(String[] args) {
+        trianglePattern(5);
 
     }
-    public static void
+    public static void trianglePattern(int number ){
+        if(number == 0){
+            return;
+        }
+        for (int i = 0; i < number ; i++) {
+            System.out.print(" * ");
+        }
+        System.out.println(" ");
+        trianglePattern(number -1 );
+
+    }
 }
