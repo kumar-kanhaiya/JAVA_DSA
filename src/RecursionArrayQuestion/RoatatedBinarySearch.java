6edce72625c04a77b04b37230252816a6c6af867
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-13 15:27:16 +0530

    code

--- src/RecursionArrayQuestion/RoatatedBinarySearch.java
+++ src/RecursionArrayQuestion/RoatatedBinarySearch.java
@@ -8,5 +8,29 @@ public class RoatatedBinarySearch {
 
 
     }
+    static int search(int[] array , int target , int s , int e){
+        if(s > e){
+            // array is not sorted
+            return -1;
+        }
+        int mid = s + (e-s)/2;
+        if(mid == target){
+            // ans found
+            return mid;
+        }
+       if( array[s] <= array[mid]){
+           // it means first half is sorted
+           if(target> array[s] && target < array[mid]){
+               return search(array,target,s,mid -1);
+           }
+           else{
+               return search(array,target,mid +1 ,e);
+           }
+       }
+       if(target >= array[mid] && target <= array[e]){
+           return search(array,target,mid +1 , e);
+       }
+       return search(array,target,s , mid-1);
+    }
 
 }
