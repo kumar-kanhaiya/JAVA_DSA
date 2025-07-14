f2e20c5a6a9d89b2607693c9ac193c4785c509e6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-14 07:01:08 +0530

    code

--- src/RecursionPatternQuestions/BubbleSort.java
+++ src/RecursionPatternQuestions/BubbleSort.java
@@ -2,6 +2,19 @@ package RecursionPatternQuestions;
 
 public class BubbleSort {
     public static void main(String[] args) {
+    int[] array = {4,3,2,1};
 
     }
+    static void bubbleSort(int[] array , int start , int end){
+        if(start > end){
+            return;
+        }
+        if(array[start] > array[start+1]){
+
+        }
+    }
+    public static void swap(int[] array , int first , int second ){
+
+    }
+
 }
