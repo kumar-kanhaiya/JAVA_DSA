6dcef60d2fc56a2e17c3bd106f37336817df64d6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-14 07:37:08 +0530

    code

--- src/RecursionPatternQuestions/SelectionSort.java
+++ src/RecursionPatternQuestions/SelectionSort.java
@@ -1,7 +1,49 @@
 package RecursionPatternQuestions;
 
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args) {
+        int[] array = {4,3,2,1};
+        selectionSort(array, array.length - 1 ,0  );
+        System.out.println(Arrays.toString(array));
 
+    }
+    public static void selectionSort(int[] array , int r , int c){
+        if(r == 0){
+            return;
+        }
+        if(c < r){
+            int max = maxIndex(array, array.length - c - 1);
+            swap(array,c, array.length - c - 1);
+            selectionSort(array,r, c+1);
+        }
+        else{
+            selectionSort(array,r - 1 , 0);
+        }
+
+    }
+
+
+    public static void swap(int[] array , int first , int second){
+        int temp = array[first];
+        array[first] =array[second];
+        array[second] =temp;
+    }
+    public static int maxIndex(int[] array , int end){
+        int ans = 0 ;
+        int max = -1;
+        for (int i = 0; i < end ; i++) {
+            if(max <array[i]){
+                max = i;
+            }
+        }
+        for (int i = 0; i <end ; i++) {
+            if(array[i] == max){
+                ans = i ;
+                break;
+            }
+        }
+        return ans;
     }
 }
