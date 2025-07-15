4f0180f3205ecae69c91e321f5d31774718b9cee
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-15 13:33:26 +0530

    code

--- src/RecursionPatternQuestions/QuickSort.java
+++ src/RecursionPatternQuestions/QuickSort.java
@@ -2,6 +2,10 @@ package RecursionPatternQuestions;
 
 public class QuickSort {
     public static void main(String[] args) {
-
+    /* 3 ways to pick the pivot
+    1. choose random element
+    2. corner element
+    3. pick the middle element
+     */
     }
 }
