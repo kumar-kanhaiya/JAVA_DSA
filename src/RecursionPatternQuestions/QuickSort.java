a82f7f31a9909a4ecf1cc7513cfccc25b180ec90
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-15 14:13:09 +0530

    code

--- src/RecursionPatternQuestions/QuickSort.java
+++ src/RecursionPatternQuestions/QuickSort.java
@@ -1,5 +1,7 @@
 package RecursionPatternQuestions;
 
+import java.util.Arrays;
+
 public class QuickSort {
     public static void main(String[] args) {
     /* 3 ways to pick the pivot
@@ -10,7 +12,39 @@ public class QuickSort {
         /* worst case and best case
         worst case is maximum or minimum present on starting or ending
         best case is middle one
+        hybrid sorting algorithm is a combination of merge sort and insertion sort
          */
+        int[] array = {5,4,3,2,1};
+        sort(array,0, array.length-1);
+        System.out.println(Arrays.toString(array));
 
+    }
+    static void sort(int[] array , int low , int high){
+        if(low >= high){
+            return;
+        }
+        int start = low;
+        int end = high ;
+
+        int mid = start + (end - start)/2;
+        int pivot = array[mid];
+        while(start <= end ){
+            while(pivot > array[start]){
+                start++;
+            }
+            while(pivot < array[end]){
+                end--;
+            }
+            if(start <= end ){
+                int temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        // now my pivot is at correct index
+        sort(array,low , end);
+        sort(array,start,high);
     }
 }
