a8ab5dd7fb146e4f5a8322ca1db3a9b1e6788f0d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-16 11:09:18 +0530

    c

--- src/RecursionStringQuestion/stringSkip.java
+++ src/RecursionStringQuestion/stringSkip.java
@@ -2,6 +2,18 @@ package RecursionStringQuestion;
 
 public class stringSkip {
     public static void main(String[] args) {
-
+    String name = "kanhaiyaapplekumarapple";
+        System.out.println(removeString(name));
+    }
+    public static String removeString(String str){
+        if(str.isEmpty()){
+            return "";
+        }
+        if(str.startsWith("apple")){
+            return removeString(str.substring(5));
+        }
+        else{
+            return str.charAt(0) + removeString(str.substring(1));
+        }
     }
 }
