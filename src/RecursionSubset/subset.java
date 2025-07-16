34527f6a3b223d876faee2081fc216f6f6bda77d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-16 12:57:12 +0530

    c

--- src/RecursionSubset/subset.java
+++ src/RecursionSubset/subset.java
@@ -2,6 +2,20 @@ package RecursionSubset;
 
 public class subset {
     public static void main(String[] args) {
+    /*
+    subset is non adjactance collections
+    the pattern of taking some element or removing some element is known as subset pattern
 
+     */
+        allSubsets("" , "abc");
+    }
+    public static void allSubsets(String p , String up){
+        if( up.isEmpty() ){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        allSubsets((p + ch),up.substring(1));
+        allSubsets(p , up.substring(1));
     }
 }
