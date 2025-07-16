55bef98f7ba24c742a8250b4d82a2972c7c1ec10
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-16 10:18:38 +0530

    code

--- src/RecursionStringQuestion/skipACharacter.java
+++ src/RecursionStringQuestion/skipACharacter.java
@@ -2,6 +2,19 @@ package RecursionStringQuestion;
 
 public class skipACharacter {
     public static void main(String[] args) {
+        String name = "kanhaiya";
+        String ans = " ";
+        remove(name , ans,0 , 'a');
+        System.out.println(ans);
 
+    }
+    public static void remove(String original , String ans , int start , char target ){
+        if(start == original.length()){
+            return;
+        }
+        if(original.charAt(start) != target){
+            ans += original.charAt(start);
+        }
+        remove(original , ans, start + 1 , target);
     }
 }
