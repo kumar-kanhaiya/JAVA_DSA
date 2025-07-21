10ec8b1cdd373da6565163d3559a7733b7c013e4
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-21 13:08:01 +0530

    c

--- src/RecursionQuestions/Leetcode17.java
+++ src/RecursionQuestions/Leetcode17.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Leetcode17 {
     public static void main(String[] args) {
-        System.out.println(letterCombinations(""));
+        System.out.println(letterCombinations2("7"));
 
     }
     public static ArrayList<String> letterCombinations(String digits) {
@@ -22,6 +22,28 @@ public class Leetcode17 {
         for (int i = (digit-1) * 3; i < digit * 3; i++) {
             char ch = (char) ('a' +  i);
             ans.addAll(check(p+ch,up.substring(1)));
+        }
+        return ans;
+    }
+
+    // second approach
+    public static ArrayList<String> letterCombinations2(String digits) {
+        return check2("",digits);
+    }
+
+    private static ArrayList<String> check2(String p, String up) {
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        String[] mapping = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        int digit = up.charAt(0) - '0'; // Convert char to int
+        String letters = mapping[digit];
+
+        for (char ch : letters.toCharArray()) {
+            ans.addAll(check2(p + ch, up.substring(1)));
         }
         return ans;
     }
