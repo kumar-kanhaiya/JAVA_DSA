4bd5a46ef30d83bf8eaabe5ca08bfb0a09fa5024
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-21 10:56:48 +0530

    code

--- src/RecursionSubset/Permutation.java
+++ src/RecursionSubset/Permutation.java
@@ -2,6 +2,20 @@ package RecursionSubset;
 
 public class Permutation {
     public static void main(String[] args) {
+        permutation("","abc");
 
+    }
+    public static void permutation(String p , String up){
+        // base condition
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        int size = p.length() + 1;
+        for (int i = 0; i < size; i++) {
+            permutation(p+ch,up.substring(1));
+            permutation(ch+p,up.substring(1));
+        }
     }
 }
