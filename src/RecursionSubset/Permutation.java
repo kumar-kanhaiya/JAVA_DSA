64469fb8e0aa6691cd1dc8eba88cf1842bd202f9
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-21 11:19:23 +0530

    code

--- src/RecursionSubset/Permutation.java
+++ src/RecursionSubset/Permutation.java
@@ -1,8 +1,11 @@
 package RecursionSubset;
 
+import java.util.ArrayList;
+
 public class Permutation {
     public static void main(String[] args) {
         permutation("","abc");
+        System.out.println(permutation2("","abc",new ArrayList<String>()));
 
     }
     public static void permutation(String p , String up){
@@ -21,5 +24,18 @@ public class Permutation {
 
     // returning array list
     // first approach pass the arraylist inside the function paratmeter
+    public static ArrayList<String> permutation2(String p , String up , ArrayList<String> list){
+        if(up.isEmpty()){
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length() ; i++) {
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+            permutation2(f + ch + s,up.substring(1),list);
 
+        }
+        return list;
+    }
 }
