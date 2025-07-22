1b5161bbac78225e9039f1a04cb7d95aa00f8d2e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-22 19:48:08 +0530

    code

--- src/RecursionQuestions/DiceProcblem.java
+++ src/RecursionQuestions/DiceProcblem.java
@@ -4,4 +4,14 @@ public class DiceProcblem {
     public static void main(String[] args) {
 
     }
+    public static void dice(String p , int target ){
+        if(target == 0){
+            System.out.println(p);
+            return;
+        }
+//        int digit = p.charAt(0) + '0';
+        for (int i = 1; i <=6 && i<= target; i++) {
+            dice(p+i,target-i);
+        }
+    }
 }
