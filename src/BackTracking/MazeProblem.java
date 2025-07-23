e1dc7a2d5e6e18ed1579c73080da3d93db208c29
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-23 08:49:31 +0530

    code

--- src/BackTracking/MazeProblem.java
+++ src/BackTracking/MazeProblem.java
@@ -67,5 +67,17 @@ public class MazeProblem {
         }
     }
 
+    static void pathWithObesticles(String p,boolean[][] maze , int r , int c ){
+        if(r==1 && c == 1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            pathWithObesticles(p + 'D' ,maze,r-1 , c);
 
+        }
+        if(c>1) {
+            pathWithObesticles(p + 'R', maze,r , c-1);
+        }
+    }
 }
