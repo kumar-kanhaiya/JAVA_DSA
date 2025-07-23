d5c6ce2dd1b72db896d64df179b35d3aa1f1163c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-23 08:21:41 +0530

    code

--- src/BackTracking/MazeProblem.java
+++ src/BackTracking/MazeProblem.java
@@ -3,6 +3,7 @@ package BackTracking;
 public class MazeProblem {
     public static void main(String[] args) {
         System.out.println(count(3,3));
+        path("",3,3);
 
     }
     static int count(int r , int c){
@@ -12,4 +13,18 @@ public class MazeProblem {
        return count(r,c-1) + count(r-1,c);
 
     }
+    static void path(String p , int r , int c ){
+        if(r==1 && c == 1){
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            path(p + 'D' ,r-1 , c);
+
+        }
+        if(c>1) {
+            path(p + 'R', r , c-1);
+        }
+    }
+
 }
