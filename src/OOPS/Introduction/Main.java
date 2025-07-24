73310ce1b4ef5eb40a1fa202a19b5bab47d05b8e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-24 08:45:21 +0530

    code

--- src/OOPS/Introduction/Main.java
+++ src/OOPS/Introduction/Main.java
@@ -1,7 +1,27 @@
 package OOPS.Introduction;
 
 public class Main {
-    public static void main(String[] args) {
+    String name;
+    int marks;
+    int rollNo;
 
+    public Main( String name,int marks, int rollNo) {
+        this.marks = marks;
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Main{" +
+                "name='" + name + '\'' +
+                ", marks=" + marks +
+                ", rollNo=" + rollNo +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Main student1 = new Main("Kanhaiya",96,5);
+        System.out.println(student1);
     }
 }
