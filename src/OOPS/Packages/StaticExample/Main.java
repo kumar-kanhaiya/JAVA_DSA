bf499ade07b153647111b3ede2493ca9c162a637
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-25 10:26:34 +0530

    code

---
+++ src/OOPS/Packages/StaticExample/Main.java
@@ -0,0 +1,7 @@
+package OOPS.Packages.StaticExample;
+
+public class Main {
+    public static void main(String[] args) {
+        Human kanhaiya = new Human(20,"kanhaiya kumar ",50,false);
+    }
+}
