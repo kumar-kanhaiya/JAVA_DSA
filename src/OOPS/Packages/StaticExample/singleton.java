29f3b7834f350514e26c729c0306d0ea66f7356b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-26 10:34:10 +0530

    code

--- src/OOPS/Packages/StaticExample/singleton.java
+++ src/OOPS/Packages/StaticExample/singleton.java
@@ -1,6 +1,13 @@
 package OOPS.Packages.StaticExample;
 
 public class singleton {
+    private singleton(){
+
+    }
+    private static singleton instance;
+    public static singleton getInstance(){
+
+    }
     public static void main(String[] args) {
 
     }
