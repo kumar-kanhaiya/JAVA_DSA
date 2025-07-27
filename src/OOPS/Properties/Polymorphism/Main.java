0d91f2aabb5e7c8f673b9469d496b4fb26b645b0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-27 10:09:16 +0530

    code

--- src/OOPS/Properties/Polymorphism/Main.java
+++ src/OOPS/Properties/Polymorphism/Main.java
@@ -3,6 +3,10 @@ package OOPS.Properties.Polymorphism;
 public class Main {
     // poly means many
     // morphism means ways to represent
+
+    // types
+    //1 compile time polymorphism / static polymorphism achieved by overloading via method overloading
+    // 2 Dynamic polymorphism   / runtime achieved by method overriding .
     public static void main(String[] args) {
         Shapes shape = new Shapes();
         shape.area();
