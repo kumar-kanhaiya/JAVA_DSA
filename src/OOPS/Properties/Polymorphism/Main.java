89cc674c6fe0e35c8360cb54f4255a9178788a00
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-27 09:51:10 +0530

    code

--- src/OOPS/Properties/Polymorphism/Main.java
+++ src/OOPS/Properties/Polymorphism/Main.java
@@ -3,4 +3,10 @@ package OOPS.Properties.Polymorphism;
 public class Main {
     // poly means many
     // morphism means ways to represent
+    public static void main(String[] args) {
+        Shapes shape = new Shapes();
+        shape.area();
+        Circle circle = new Circle();
+        circle.area();
+    }
 }
--- src/OOPS/Properties/Polymorphism/Rectangle.java
+++ src/OOPS/Properties/Polymorphism/Rectangle.java
@@ -3,5 +3,6 @@ package OOPS.Properties.Polymorphism;
 public class Rectangle extends Shapes{
     void area(){
         System.out.println("Area of Rectangle  is length * breadth ");
+
     }
 }
