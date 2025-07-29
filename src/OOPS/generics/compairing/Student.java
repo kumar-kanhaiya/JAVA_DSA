b3b9f3c4db38382a957e2a24e23cc80e39311713
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-29 11:23:49 +0530

    code

--- src/OOPS/generics/compairing/Student.java
+++ src/OOPS/generics/compairing/Student.java
@@ -1,10 +1,28 @@
 package OOPS.generics.compairing;
 
-public class Student {
+public class Student implements Comparable<Student> {
     int rollNo ;
     float marks;
     public Student(int rollNo , float marks){
         this.rollNo = rollNo;
         this.marks = marks;
+    }
+//    @Override
+//    public int compareTo(Student o){
+//
+//    }
+
+    public static void main(String[] args) {
+        Student kanhaiya = new Student(12,92.5f);
+        Student rahul = new Student(11,94.6f);
+        if(kanhaiya.compareTo(rahul) <0){
+            System.out.println("not equal");
+        }
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        int difference = (int)(this.marks - o.marks);
+        return difference;
     }
 }
