98a757671a04129bc41dd2353ad9b92adcbc7dbd
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-29 10:25:03 +0530

    code

---
+++ src/OOPS/generics/compairing/Student.java
@@ -0,0 +1,10 @@
+package OOPS.generics.compairing;
+
+public class Student {
+    int rollNo ;
+    float marks;
+    public Student(int rollNo , float marks){
+        this.rollNo = rollNo;
+        this.marks = marks;
+    }
+}
