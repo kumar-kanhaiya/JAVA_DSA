77fb2b33ac7d7722f3715b3b6e2069e940548ec0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-29 09:37:56 +0530

    code

--- src/OOPS/generics/CustomArraylist.java
+++ src/OOPS/generics/CustomArraylist.java
@@ -4,6 +4,35 @@ import java.util.ArrayList;
 
 public class CustomArraylist {
 
+        private int[] data;
+        private static int DEFAULT_SIZE = 10;
+        private int size = 0;
+
+        public CustomArraylist(){
+            this.data = new int[DEFAULT_SIZE];
+
+        }
+
+        public void add(int num ){
+            if(isFull()){
+                resize();
+            }
+            data[size++] = num;
+        }
+
+    private void resize() {
+            int[] temp = new int[data.length*2];
+            // copy the current items in the array
+        for (int i = 0; i < data.length ; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
+    }
+
+    private boolean isFull() {
+            return size == data.length;
+            
+    }
 
     public static void main(String[] args) {
         ArrayList list = new ArrayList();
