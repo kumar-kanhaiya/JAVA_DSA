b1739ebba3da3b8ebb8dcc0f9cb846c35ae72cc2
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-29 09:31:56 +0530

    code

--- src/OOPS/generics/CustomArraylist.java
+++ src/OOPS/generics/CustomArraylist.java
@@ -1,7 +1,17 @@
 package OOPS.generics;
 
+import java.util.ArrayList;
+
 public class CustomArraylist {
+
+
     public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add(45);
+        list.get(0);
+        list.remove(0);
+        list.set(1,6585);
+        list.size();
 
     }
 }
