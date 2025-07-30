4c92364cbeaa7b9fb9153eaeb851708d4063e543
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-30 19:47:24 +0530

    code

--- src/OOPS/EnumsExample/Basics.java
+++ src/OOPS/EnumsExample/Basics.java
@@ -1,7 +1,19 @@
 package OOPS.EnumsExample;
 
 public class Basics {
+    enum Week {
+        // these are enum constant
+        Monday , Tuesday , Wednesday , Thursday , Friday , Saturday , Sunday
+        // public static and final
+        // since it is final you cant create child enum
+        // type is week
+    }
     public static void main(String[] args) {
         // enums : - it is a group of variable it is just like constant immutable
+        Week week ;
+        week = Week.Monday;
+        for(Week day : Week.values()){
+            System.out.println(day);
+        }
     }
 }
