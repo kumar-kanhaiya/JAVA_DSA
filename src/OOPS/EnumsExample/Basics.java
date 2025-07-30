b1a1369468bb949199c6a6349e860bae819d3c4b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-30 19:43:38 +0530

    code

--- src/OOPS/EnumsExample/Basics.java
+++ src/OOPS/EnumsExample/Basics.java
@@ -1,6 +1,6 @@
-package OOPS.Enums;
+package OOPS.EnumsExample;
 
-public class enumms {
+public class Basics {
     public static void main(String[] args) {
         // enums : - it is a group of variable it is just like constant immutable
     }
