5dd71c9631ad7d235506060e0449d59958a7bb86
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-31 11:15:19 +0530

    code

--- src/LinkedList/CustomLinkedList.java
+++ src/LinkedList/CustomLinkedList.java
@@ -20,6 +20,18 @@ public class CustomLinkedList {
         }
 
     }
+    public void insertLast(int value){
+        if(tail == null){
+            insertFirst(value);
+            return;
+        }
+        Node node = new Node(value);
+        tail.next = node;
+        tail = node;
+        size++;
+
+    }
+
     public void display(){
         Node temp = head;
         while (temp!= null){
