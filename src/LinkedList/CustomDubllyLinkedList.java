f24ed9cd385a73ff1c162d65e70ac6ef825f390d
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-31 12:37:03 +0530

    code

--- src/LinkedList/CustomDubllyLinkedList.java
+++ src/LinkedList/CustomDubllyLinkedList.java
@@ -4,18 +4,22 @@ public class CustomDubllyLinkedList {
 
     private Node head ;
     private Node tail;
+    private int size;
+    private Node pre;
 
-    class Node{
-        private int value;
-        private Node next;
+    private class Node{
+        int value;
+         Node next;
+         Node prev;
 
         public Node(int value) {
             this.value = value;
         }
 
-        public Node(int value, Node next) {
+        public Node(int value, Node next, Node prev) {
             this.value = value;
             this.next = next;
+            this.prev = prev;
         }
     }
 }
