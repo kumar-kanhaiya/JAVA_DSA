048bcb1fdd87ed121db222a19aded5a98d6cad8a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-31 11:05:16 +0530

    code

--- src/LinkedList/Basic.java
+++ src/LinkedList/Basic.java
@@ -5,6 +5,11 @@ package LinkedList;
 public class Basic {
     public static void main(String[] args) {
     CustomLinkedList list = new CustomLinkedList();
+    list.insertFirst(3);
+    list.insertFirst(8);
+    list.insertFirst(7);
+    list.insertFirst(14);
+    list.insertFirst(10);
 
     }
 }
