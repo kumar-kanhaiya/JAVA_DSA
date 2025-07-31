3457ca201c1b7f40a63a5eba5eea4bc6c3df92ba
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-07-31 10:24:35 +0530

    code

--- src/LinkedList/Basic.java
+++ src/LinkedList/Basic.java
@@ -1,7 +1,10 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class Basic {
     public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
 
     }
 }
---
+++ src/LinkedList/CustomLinkedList.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class CustomLinkedList {
+
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public  CustomLinkedList(){
+        this.size = 0;
+    }
+
+    private  class Node{
+        private int value ;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+
+
+}
