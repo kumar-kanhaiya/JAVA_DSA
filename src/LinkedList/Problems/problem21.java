c965d487aea2f497a406acfe3b524fe1047170a8
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-01 11:28:37 +0530

    code

--- src/LinkedList/Problems/problem21.java
+++ src/LinkedList/Problems/problem21.java
@@ -9,39 +9,70 @@ public class problem21 {
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
   }
 
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = new ListNode();
-        while(list1 != null  || list2 != null){
-            if(list1.val == list2.val){
-                ListNode node = new ListNode(list1.val);
-                node.next = head;
-                head = node;
-                ListNode node2 = new ListNode(list2.val);
-                node2.next = head;
-                head = node2;
-                list1 = list1.next;
-                list2 = list2.next;
-            }
-             if(list1.val > list2.val){
-                ListNode node = new ListNode(list1.val);
-                node.next = head;
-                head = node;
-                list1 = list1.next;
-            }
-            if(list2.val > list1.val){
-                ListNode node2 = new ListNode(list2.val);
-                node2.next = head;
-                head = node2;
-                list2 = list2.next;
-            }
-        }
-        return head;
-    }
-    public  void insertElement( ListNode head ,int val ){
-          ListNode node  = new ListNode(val);
-          node.next = head;
-          head = node;
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        ListNode f = list1;
+//        ListNode s = list2;
+//        ListNode ans = new ListNode();
+//        ListNode tail = ans;
+//        while(list1 != null && list2 != null){
+//            if(list1.val <= list2.val){
+//                tail.next = new ListNode(list1.val);
+//                list1 = list1.next;
+//            }
+//            else{
+//                tail.next = new ListNode(list2.val);
+//
+//                list2 = list2.next;
+//            }
+//            tail = tail.next;
+//        }
+//        while(list1 != null){
+//            tail.next = new ListNode(list1.val);
+//            list1 = list1.next;
+//            tail = tail.next;
+//        }
+//        while(list2 != null){
+//            tail.next = new ListNode(list2.val);
+//            list2 = list2.next;
+//            tail = tail.next;
+//        }
+//        return ans.next;
+//    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+          ListNode f = list1;
+          ListNode s = list2;
+          ListNode ans = new ListNode();
+          while(f!= null && s != null){
+              if(f.val < s.val){
+                  ans = new ListNode(f.val);
+                  f= f.next;
+                  ans = ans.next;
 
+              }
+              if(s.val < f.val){
+                  ans = new ListNode(s.val);
+                  s= s.next;
+                  ans = ans.next;
+              }
+              if(s.val == f.val){
+                  ans = new ListNode(f.val);
+                  f = f.next;
+                  ans = ans.next;
+              }
+              ans = ans.next;
+          }
+          // checking
+        while(f!= null){
+            ans = new ListNode(f.val);
+            f = f.next;
+
+        }
+        while (s!= null){
+            ans = new ListNode(s.val);
+            s = s.next;
+
+        }
+        return ans;
     }
 
 }
