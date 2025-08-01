3131a607712aa8c48b5847e0d97fb471f3dccb06
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-01 07:32:49 +0530

    code

--- src/LinkedList/CircularLinkList.java
+++ src/LinkedList/CircularLinkList.java
@@ -1,5 +1,17 @@
 package LinkedList;
 
 public class CircularLinkList {
+    private class Node{
+        private int val;
+        private int next;
 
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, int next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
