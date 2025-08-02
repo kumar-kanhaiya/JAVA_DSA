e702968abeb7ec5dbbe6767a9c0b8c2ebd0e001a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-02 09:43:38 +0530

    code

--- src/LinkedList/Problems/problem21.java
+++ src/LinkedList/Problems/problem21.java
@@ -9,36 +9,37 @@ public class problem21 {
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
   }
 
-//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-//        ListNode f = list1;
-//        ListNode s = list2;
-//        ListNode ans = new ListNode();
-//        ListNode tail = ans;
-//        while(list1 != null && list2 != null){
-//            if(list1.val <= list2.val){
-//                tail.next = new ListNode(list1.val);
-//                list1 = list1.next;
-//            }
-//            else{
-//                tail.next = new ListNode(list2.val);
-//
-//                list2 = list2.next;
-//            }
-//            tail = tail.next;
-//        }
-//        while(list1 != null){
-//            tail.next = new ListNode(list1.val);
-//            list1 = list1.next;
-//            tail = tail.next;
-//        }
-//        while(list2 != null){
-//            tail.next = new ListNode(list2.val);
-//            list2 = list2.next;
-//            tail = tail.next;
-//        }
-//        return ans.next;
-//    }
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode ans = new ListNode();
+        ListNode tail = ans;
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                tail.next = list1;
+                tail = tail.next;
+                list1 = list1.next;
+            }
+            else{
+                tail.next = list2;
+
+                list2 = list2.next;
+                tail = tail.next;
+            }
+
+        }
+        while(list1 != null){
+            tail.next = list1;
+            list1 = list1.next;
+            tail = tail.next;
+        }
+        while(list2 != null){
+            tail.next = list2;
+            list2 = list2.next;
+            tail = tail.next;
+        }
+        return ans.next;
+    }
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2){
           ListNode f = list1;
           ListNode s = list2;
           ListNode ans = new ListNode();
