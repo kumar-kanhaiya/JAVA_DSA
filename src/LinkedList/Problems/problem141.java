6274ad14ea36c666181a0a86015a6584608077a0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-02 10:22:36 +0530

    code

--- src/LinkedList/Problems/problem141.java
+++ src/LinkedList/Problems/problem141.java
@@ -1,5 +1,7 @@
 package LinkedList.Problems;
 
+import java.util.List;
+
 public class problem141 {
     class ListNode {
           int val;
@@ -24,6 +26,21 @@ public class problem141 {
             return false;
         }
 
-        // f
+        // finding the length of the cycle
+        public int lengthCycle(ListNode head){
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while(fast!= null && fast.next != null){
+                fast = fast.next.next;
+                slow= slow.next;
+
+                if(fast == slow){
+                    ListNode temp = slow;
+
+                }
+            }
+            return -1;
+        }
 
 }
