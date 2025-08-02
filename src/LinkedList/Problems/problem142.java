576ac82c1dd993b18513522bdd7a7e17fd1209e1
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-02 10:45:13 +0530

    code

--- src/LinkedList/Problems/problem142.java
+++ src/LinkedList/Problems/problem142.java
@@ -10,4 +10,37 @@ public class problem142 {
           }
     }
 
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+
+        while(fast!= null && fast.next!= null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                // ListNode Entry = head;
+                // while(Entry != slow){
+                //     Entry = Entry.next;
+                //     slow = slow.next;
+                // }
+
+                ListNode temp = slow;
+                ListNode ans = new ListNode(0);
+                ListNode dummy = ans;
+                do{
+                    dummy.next = new ListNode(temp.val);
+                    dummy = dummy.next;
+                    temp =temp.next;
+
+
+                }while(temp!= slow);
+                return ans.next;
+            }
+
+        }
+        return null;
+    }
+
 }
