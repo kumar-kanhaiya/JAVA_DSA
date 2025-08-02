d1ac342ba9b765d53bc6b53d6853df4eed9b512b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-02 10:27:18 +0530

    code

--- src/LinkedList/Problems/problem142.java
+++ src/LinkedList/Problems/problem142.java
@@ -1,5 +1,13 @@
 package LinkedList.Problems;
 
 public class problem142 {
+    class ListNode {
+         int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+          }
+    }
 
 }
