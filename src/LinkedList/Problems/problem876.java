7eb76e1227af5532b29d8d9c3b497f6d9c192194
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-02 11:50:15 +0530

    code

--- src/LinkedList/Problems/problem876.java
+++ src/LinkedList/Problems/problem876.java
@@ -14,7 +14,39 @@ public class problem876 {
           this.val = val; this.next = next;
       }
   }
+    public int lengthCycle(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast!= null && fast.next != null){
+            fast = fast.next.next;
+            slow= slow.next;
+
+            if(fast == slow){
+                ListNode temp = slow;
+                int length = 0;
+                do {
+                    temp = temp.next;
+                    length++;
+                }while(temp != slow);
+
+                return length;
+            }
+        }
+        return -1;
+    }
     public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        int length = 0;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow =slow.next;
+
+        }
+        return slow;
+
 
     }
 
