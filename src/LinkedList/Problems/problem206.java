b5b810e20f9ec722d6447d174802bb2cdbc4f12b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-03 11:40:29 +0530

    code

--- src/LinkedList/Problems/problem206.java
+++ src/LinkedList/Problems/problem206.java
@@ -11,11 +11,30 @@ public class problem206 {
           this.val = val;
       }
       ListNode(int val, ListNode next) {
-          this.val = val; this.next = next;
+          this.val = val;
+          this.next = next;
       }
     }
+    // first we done without using recursion
     public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        ListNode present = head;
+        ListNode next = present.next;
+        ListNode prev = null;
 
+        while(present != null){
+            present.next = prev;
+            prev = present;
+            present = next;
+            if(next != null){
+                next = next.next;
+            }
+
+        }
+
+        return prev;
     }
 
 
