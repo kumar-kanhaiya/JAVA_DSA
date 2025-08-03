6589e9db210043967ee906d36f528f32cc86bba6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-03 12:19:35 +0530

    code

--- src/LinkedList/Problems/problem92.java
+++ src/LinkedList/Problems/problem92.java
@@ -15,11 +15,11 @@ public class problem92 {
             this.next = next;
         }
     }
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(head == null){
+    public ListNode reverseBetween2(ListNode head, int left, int right) {
+        if(left == right){
             return head;
         }
-
+        ListNode cureent = head;
         ListNode present = get(head,left);
         ListNode prev = null;
         ListNode next = present.next;
@@ -43,5 +43,41 @@ public class problem92 {
             node = node.next;
         }
         return null;
+    }
+
+    // second approach
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left == right){
+            return head;
+        }
+        ListNode current = head;
+        ListNode prev = null;
+        for (int i = 0; current != null && i < left-1 ; i++) {
+            prev = current;
+            current = current.next;
+
+        }
+        ListNode last  = prev;
+        ListNode newEnd = current;
+
+        // reveree between left and right
+        ListNode next = current.next;
+        for (int i = 0; current!= null && i < right - left +1  ; i++) {
+                current.next = prev;
+                prev = current;
+                current = next;
+                if(next != null){
+                    next = next.next;
+                }
+        }
+        if(last != null){
+            last.next = prev;
+
+        }
+        else{
+            head = prev;
+        }
+        newEnd.next = current;
+        return head;
     }
 }
