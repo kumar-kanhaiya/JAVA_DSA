e26bfe1ceb5863673ce817dd1bb72e4704ae761b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-03 13:06:56 +0530

    code

--- src/LinkedList/Problems/problem143.java
+++ src/LinkedList/Problems/problem143.java
@@ -1,4 +1,21 @@
 package LinkedList.Problems;
 
 public class problem143 {
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {
+
+      }
+      ListNode(int val) {
+          this.val = val;
+      }
+      ListNode(int val, ListNode next) {
+          this.val = val; this.next = next;
+      }
+  }
+    public void reorderList(ListNode head) {
+
+    }
+
 }
