4a915d941e116cecbe77f28c6e4c5d85ad00b1f0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-03 11:13:22 +0530

    code

--- src/LinkedList/CustomLinkedList.java
+++ src/LinkedList/CustomLinkedList.java
@@ -178,5 +178,23 @@ public class CustomLinkedList {
         }
     }
 
+    // reverse using recursion
+    private void reverse(Node node ){
+        if(node == tail){
+            head = tail;
+            return;
+        }
+
+        reverse(node.next);
+        tail.next = node;
+        tail = node;
+        tail.next= null;
+    }
+
+    // in place reversal of linked list
+
+
+
+
 
 }
