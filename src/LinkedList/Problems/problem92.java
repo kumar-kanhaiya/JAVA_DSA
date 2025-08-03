bd779dcd6e4225ffe472058276e4a6d63ba6ebd2
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-03 11:57:08 +0530

    code

--- src/LinkedList/Problems/problem92.java
+++ src/LinkedList/Problems/problem92.java
@@ -3,19 +3,42 @@ package LinkedList.Problems;
 public class problem92 {
     public class ListNode {
         int val;
-        problem206.ListNode next;
+        ListNode next;
         ListNode() {
 
         }
         ListNode(int val) {
             this.val = val;
         }
-        ListNode(int val, problem206.ListNode next) {
+        ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
         }
     }
     public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null){
+            return head;
+        }
 
+        ListNode present = get(head,left);
+        ListNode prev = null;
+        ListNode next = present.next;
+
+        while(present != get(head,right)){
+            present.next = prev;
+            prev = present;
+            present = next;
+            if(next != get(head,right)){
+                next = next.next;
+            }
+        }
+
+    }
+    private ListNode get( ListNode head, int index){
+        ListNode node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
     }
 }
