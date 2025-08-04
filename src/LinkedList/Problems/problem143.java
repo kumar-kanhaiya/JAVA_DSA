18348d0f1f548407ef7eac7c105a9d19e5179721
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-04 10:59:38 +0530

    code

--- src/LinkedList/Problems/problem143.java
+++ src/LinkedList/Problems/problem143.java
@@ -15,7 +15,31 @@ public class problem143 {
       }
   }
     public void reorderList(ListNode head) {
+        ListNode slow = head ;
+        ListNode fast = head;
+        // finding the middle node
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
 
+        }
+        // we get the middle element then reverse the node
+
+    }
+    public ListNode reverese(ListNode head){
+          ListNode present = head;
+          ListNode prev = null;
+          ListNode next = present.next;
+
+          while (present.next != null ){
+              present.next = prev;
+              prev = present;
+              present = next;
+              if(next != null){
+                  next = next.next;
+              }
+          }
+          return prev;
     }
 
 }
