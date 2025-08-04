4624d1d62df73ba4028367b5839b402e9e020b5b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-04 11:57:06 +0530

    code

--- src/LinkedList/Problems/problem25.java
+++ src/LinkedList/Problems/problem25.java
@@ -14,6 +14,43 @@ public class problem25 {
           this.val = val; this.next = next;
       }
   }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(k<1 || head == null){
+            return head;
+        }
+        ListNode current = head;
+        ListNode prev = null;
 
+        while(true){
+            ListNode last  = prev;
+            ListNode newEnd = current;
+
+            // reveree between left and right
+            ListNode next = current.next;
+            for (int i = 0; current!= null && i < k ; i++) {
+                current.next = prev;
+                prev = current;
+                current = next;
+                if(next != null){
+                    next = next.next;
+                }
+            }
+            if(last != null){
+                last.next = prev;
+
+            }
+            else{
+                head = prev;
+            }
+            newEnd.next = current;
+            if(current == null){
+                break;
+            }
+            prev = newEnd;
+
+        }
+        return head;
+
+    }
 
 }
