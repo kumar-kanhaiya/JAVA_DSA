5cba20baeae0ca6557940656cc32a77877ea4861
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-04 11:32:48 +0530

    code

---
+++ src/LinkedList/Problems/problem25.java
@@ -0,0 +1,19 @@
+package LinkedList.Problems;
+
+public class problem25 {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {
+
+      }
+      ListNode(int val) {
+          this.val = val;
+      }
+      ListNode(int val, ListNode next) {
+          this.val = val; this.next = next;
+      }
+  }
+
+
+}
