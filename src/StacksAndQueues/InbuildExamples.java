98e9c461164579da6817ab20500507a76d3f5248
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-05 19:41:05 +0530

    code

--- src/StacksAndQueues/InbuildExamples.java
+++ src/StacksAndQueues/InbuildExamples.java
@@ -1,5 +1,7 @@
 package StacksAndQueues;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class InbuildExamples {
@@ -18,5 +20,19 @@ public class InbuildExamples {
         System.out.println(stack.pop());
         System.out.println(stack.pop());
         System.out.println(stack.pop());
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(4);
+        queue.add(12);
+        queue.add(17);
+        queue.add(6);
+        queue.add(11);
+        // it works on the FIFO principle - First In First Out ;
+        System.out.println("QUEUE:-");
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
     }
 }
