bf35e404f5e9f2fd444b57d384653f7a09580636
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-05 20:20:47 +0530

    code

--- src/StacksAndQueues/CustomStack.java
+++ src/StacksAndQueues/CustomStack.java
@@ -1,5 +1,46 @@
 package StacksAndQueues;
 
 public class CustomStack {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
 
+    int ptr = -1;
+
+    public CustomStack(){
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomStack(int size) {
+        this.data = new int[size];
+    }
+    public boolean push(int item ){
+        if(isFull()){
+            System.out.println("Stack is Full !!");
+            return false;
+        }
+        ptr++;
+        data[ptr] = item;
+        return true;
+    }
+
+    private boolean isFull() {
+        // it means pointer is at last position
+        return ptr == data.length - 1;
+
+    }
+
+    public boolean isEmpty(){
+        return ptr == -1;
+    }
+
+    public int pop() throws Exception{
+        if(isEmpty()){
+            throw new Exception("Cannot pop from an empty stack ");
+        }
+//        int removed = data[ptr];
+//        ptr--;
+//        return removed;
+        // in simple word
+        return data[ptr--];
+    }
 }
