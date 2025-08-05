a0417229829e710b345f3eeaef15b0dc13a8fe2c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-05 20:29:01 +0530

    code

---
+++ src/StacksAndQueues/StackMain.java
@@ -0,0 +1,22 @@
+package StacksAndQueues;
+
+public class StackMain {
+    public static void main(String[] args) throws StackException {
+        CustomStack stack = new CustomStack(5);
+        stack.push(12);
+        stack.push(1);
+        stack.push(16);
+        stack.push(19);
+        stack.push(21);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+
+
+    }
+}
