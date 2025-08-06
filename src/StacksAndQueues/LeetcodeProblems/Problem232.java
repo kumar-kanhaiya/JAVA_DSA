a7a7254b77e3a278fe2cfb58ffb26cd29ddb68bd
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-06 10:48:37 +0530

    code

---
+++ src/StacksAndQueues/LeetcodeProblems/Problem232.java
@@ -0,0 +1,27 @@
+package StacksAndQueues.LeetcodeProblems;
+
+public class Problem232 {
+    class MyQueue{
+
+
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int peek() {
+
+    }
+
+    public boolean empty() {
+
+    }
+    }
+}
