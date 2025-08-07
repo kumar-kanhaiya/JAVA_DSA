f18156efb78720a065a436bff984e2719408138b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-07 09:16:38 +0530

    code

--- src/StacksAndQueues/LeetcodeProblems/Problem232.java
+++ src/StacksAndQueues/LeetcodeProblems/Problem232.java
@@ -1,27 +1,49 @@
 package StacksAndQueues.LeetcodeProblems;
 
+import java.util.Stack;
+
 public class Problem232 {
     class MyQueue{
+        private Stack<Integer> first;
+        private Stack<Integer> second;
 
 
     public MyQueue() {
+        first = new Stack<>();
+        second = new Stack<>();
+
 
     }
 
     public void push(int x) {
-
+        first.push(x);
     }
 
-    public int pop() {
-
+    public int pop() throws Exception {
+        while(!first.isEmpty()){
+            second.push(first.pop());
+        }
+        int removed = second.pop();
+        while(!second.isEmpty()){
+            first.push(second.pop());
+        }
+        return removed;
     }
 
-    public int peek() {
+    public int peek() throws Exception {
+        while(!first.isEmpty()){
+            second.push(first.pop());
+        }
+        int peeked = second.peek();
+        while(!second.isEmpty()){
+            first.push(second.pop());
+        }
+        return peeked;
 
     }
 
     public boolean empty() {
-
+        return first.isEmpty();
     }
     }
 }
