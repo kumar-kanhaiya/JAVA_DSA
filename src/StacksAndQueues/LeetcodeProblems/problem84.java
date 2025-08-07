9e5b04ca51697fce4f991e09efa8ec97ee4651e5
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-07 11:55:27 +0530

    code

--- src/StacksAndQueues/LeetcodeProblems/problem84.java
+++ src/StacksAndQueues/LeetcodeProblems/problem84.java
@@ -1,5 +1,26 @@
 package StacksAndQueues.LeetcodeProblems;
 
+import java.util.Stack;
+
 public class problem84 {
+    public static void main(String[] args) {
+
+    }
+    public static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+
+        stack.push(0);
+
+        for (int i = 1; i < heights.length ; i++) {
+                while(!stack.isEmpty() && heights[i] < heights[stack.peek()] ){
+                    max = getMax(heights , stack ,max , i);
+                }
+                stack.push(i);
+        }
+
+
+        return max;
+    }
 
 }
