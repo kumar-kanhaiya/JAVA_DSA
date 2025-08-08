5db65b25ad68ef718ab11a553ae74bbf4548e97b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-08 09:14:26 +0530

    code

--- src/StacksAndQueues/LeetcodeProblems/problem20.java
+++ src/StacksAndQueues/LeetcodeProblems/problem20.java
@@ -1,5 +1,10 @@
 package StacksAndQueues.LeetcodeProblems;
 
 public class problem20 {
+    public static void main(String[] args) {
 
+    }
+    public static boolean isValid(String s) {
+
+    }
 }
