76049a2bf8788c28e72b4d743818faffe72a2e9c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-08 09:30:08 +0530

    code

--- src/StacksAndQueues/LeetcodeProblems/problem20.java
+++ src/StacksAndQueues/LeetcodeProblems/problem20.java
@@ -1,10 +1,34 @@
 package StacksAndQueues.LeetcodeProblems;
 
+import java.util.Stack;
+
 public class problem20 {
     public static void main(String[] args) {
+        System.out.println(isValid("[]()"));
 
     }
     public static boolean isValid(String s) {
-
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }
+            else{
+                if(ch == ')'){
+                    if(stack.peek() != '(' || stack.isEmpty()){
+                        return false;
+                    }
+                }if(ch == ']'){
+                    if(stack.peek() != '[' || stack.isEmpty()){
+                        return false;
+                    }
+                }if(ch == '}'){
+                    if(stack.peek() != '{' || stack.isEmpty()){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
