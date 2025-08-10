6f5d550b2eded6071d0eace1bb585a3ed533c6af
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-10 11:31:02 +0530

    modifiy display

--- src/Tree/BinaryTree.java
+++ src/Tree/BinaryTree.java
@@ -60,10 +60,33 @@ public class BinaryTree {
         display(node.right ,  indent + "\t");
     }
 
+    public void prettyDisplay(){
+        prettyDisplay(root , 0);
+    }
+    private void prettyDisplay(Node node , int level ){
+        if(node == null){
+            return;
+        }
+        prettyDisplay(node.right , level+1);
+        if(level != 0){
+            for (int i = 0; i <level - 1 ; i++) {
+                System.out.print("|\t\t");
+
+            }
+            System.out.println("|------>" + node.value);
+        }
+        else{
+            System.out.println(node.value);
+        }
+        prettyDisplay(node.left , level+1);
+
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BinaryTree tree = new BinaryTree();
         tree.populate(scanner);
         tree.display();
     }
+
 }
