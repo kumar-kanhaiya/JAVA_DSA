adce39707ebac0114f40462db220d7875f7d8b0e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-10 20:37:26 +0530

    code

--- src/Tree/BinarySearchTree.java
+++ src/Tree/BinarySearchTree.java
@@ -46,4 +46,24 @@ public class BinarySearchTree {
         display(node.left , "Left child of " + node.getValue() + " : ");
         display(node.right , "Right child of " + node.getValue() + " : ");
     }
+
+    public void insert(int value ){
+
+    }
+    private Node insert(int value , Node node){
+        if(node == null){
+            node = new Node(value);
+            return node;
+        }
+        if(value < node.value){
+            node.left = insert(value , node.left);
+        }
+        if(value > node.value){
+            node.right = insert(value , node.right);
+        }
+
+        node.height = Math.max(height(node.left) , height(node.right)) + 1;
+
+        return node;
+    }
 }
