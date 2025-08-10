411a09125d13432cb59ef8fcb79b1a623822fffc
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-10 12:32:34 +0530

    code;' g

--- src/Tree/BinarySearchTree.java
+++ src/Tree/BinarySearchTree.java
@@ -1,5 +1,49 @@
 package Tree;
 
 public class BinarySearchTree {
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
 
+
+        public Node(int value) {
+            this.value = value;
+        }
+        public int getValue(){
+            return value;
+        }
+
+    }
+    private Node root;
+
+    public BinarySearchTree(){
+
+    }
+    public int height(Node node){
+        if(node == null){
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty(){
+        if(root == null){
+            return true;
+        }
+        return false;
+    }
+
+    public void display(){
+        display(root , "Root Node: ");
+    }
+    private void display(Node node , String details){
+        if(node == null){
+            return;
+        }
+        System.out.println(details + node.getValue());
+        display(node.left , "Left child of " + node.getValue() + " : ");
+        display(node.right , "Right child of " + node.getValue() + " : ");
+    }
 }
