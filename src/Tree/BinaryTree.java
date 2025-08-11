30f063d68125e874f933061d12eea7c4a9d5998f
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-11 09:30:19 +0530

    code

--- src/Tree/BinaryTree.java
+++ src/Tree/BinaryTree.java
@@ -82,6 +82,42 @@ public class BinaryTree {
 
     }
 
+    public void preOrder(){
+        preOrder(root);
+    }
+    private void preOrder(Node node){
+        if(node == null){
+            return;
+        }
+        // print the node than go to left and go to right
+        System.out.print(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void postOrder(){
+        postOrder(root);
+    }
+    private void postOrder(Node node){
+        if(node == null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.value + " ");
+    }
+    public void inOrder(){
+        inOrder(root);
+    }
+    private void inOrder(Node node){
+        if(node == null){
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.value + " ");
+        inOrder(node.right);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BinaryTree tree = new BinaryTree();
