3f39f1a18e91f3dd0a6053e6518b6c167ef056c4
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-12 10:55:21 +0530

    right rotate

--- src/Tree/AVLTree.java
+++ src/Tree/AVLTree.java
@@ -101,6 +101,21 @@ public class AVLTree {
         return node;
     }
 
+    public Node rightRotate(Node p){
+        Node c = p.left;
+        Node t = c.right;
+        c.right = p;
+        p.left = t;
+
+        p.height = Math.max(height(p.left) , height(p.right) + 1);
+        c.height = Math.max(height(c.left) , height(c.right) + 1);
+
+        return c;
+    }
+    public Node leftRotate(Node node){
+
+    }
+
     public boolean balanced() {
         return balanced(root);
     }
