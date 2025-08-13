9c6112563117953605f91e078a485666a2e64da6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-13 10:56:00 +0530

    code

--- src/Tree/SegmentTree.java
+++ src/Tree/SegmentTree.java
@@ -5,6 +5,8 @@ public class SegmentTree {
 
     }
     public static void main(String[] args) {
+        int[] array = {3,8,6,7,-2,-8,4,9};
+        SegmentTree tree = new SegmentTree(array);
 
     }
     static class Node{
@@ -20,8 +22,13 @@ public class SegmentTree {
             this.endInterval = endInterval;
 
         }
-        Node root;
-
+    }
+    Node root;
+    public SegmentTree(int[] array ){
+        // creating the tree by using this array
+        this.root = constructTree(array, 0 , array.length - 1);
+    }
+    public Node constructTree(int[] array , int startInterval , int endInterval){
 
     }
 
