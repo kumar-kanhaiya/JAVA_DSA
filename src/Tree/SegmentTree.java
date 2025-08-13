7ed39694187edf8ff8744521d2534b954a0d1e93
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-13 10:45:29 +0530

    code

--- src/Tree/SegmentTree.java
+++ src/Tree/SegmentTree.java
@@ -1,5 +1,13 @@
 package Tree;
 
 public class SegmentTree {
+    static class Node{
+        int data;
+        int startInterval ;
+        int end ;
+        Node left ;
+        Node right ;
+
+    }
 
 }
