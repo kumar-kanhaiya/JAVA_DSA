597efaaafce85a16284ce6626e9d86d33cdc570e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-20 17:30:12 +0530

    code

--- src/StringBuffer/LargeNumbers.java
+++ src/StringBuffer/LargeNumbers.java
@@ -1,5 +1,7 @@
 package StringBuffer;
 
+import com.sun.security.jgss.GSSUtil;
+
 import java.math.BigInteger;
 
 public class LargeNumbers {
@@ -10,6 +12,7 @@ public class LargeNumbers {
         BigInteger A = BigInteger.valueOf(33);
         BigInteger B = BigInteger.valueOf(3384);
         BigInteger C = new BigInteger("468454");
+        BigInteger F = new BigInteger("46845454658468");
 
         // constant
         BigInteger D = BigInteger.ONE;
@@ -18,5 +21,18 @@ public class LargeNumbers {
         BigInteger s = A.add(B);
         System.out.println(s);
 
+        // multiplication
+        BigInteger m = C.multiply(F);
+        System.out.println(m);
+
     }
+    static BigInteger fact(int num){
+        BigInteger ans = new BigInteger("`1");
+
+        for (int i = 2; i <= num ; i++) {
+            ans = ans.multiply(BigInteger.valueOf(i));
+        }
+        return ans;
+    }
+
 }
