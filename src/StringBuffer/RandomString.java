814f0049c239a78e320a1a39d3631dcb2c3b3258
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-20 16:29:43 +0530

    generateRandom

--- src/StringBuffer/RandomString.java
+++ src/StringBuffer/RandomString.java
@@ -2,6 +2,22 @@ package StringBuffer;
 
 public class RandomString {
     public static void main(String[] args) {
+        System.out.println(generate(5));
 
     }
+
+    static String generate(int size ){
+        StringBuffer str = new StringBuffer();
+        while(size>0) {
+
+
+            int randomNumber = (int) (Math.random() * 25) + 1;
+
+            str.append((char)('a' + randomNumber));
+            size--;
+
+        }
+        return str.toString();
+    }
+
 }
