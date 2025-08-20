68b4a2a178a9b75a562be1cd846dcedde02f5dcb
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-20 15:35:17 +0530

    constructor -1

--- src/StringBuffer/Main.java
+++ src/StringBuffer/Main.java
@@ -2,6 +2,18 @@ package StringBuffer;
 
 public class Main {
     public static void main(String[] args) {
+        // Constructor -1
+        StringBuffer sb = new StringBuffer();
+        sb.append("Kanhaiya Kumar Singh ");
+        String str = sb.toString();
+        System.out.println(str);
 
+//        Advantage over String
+//        Mutable
+//        Efficient
+//        Thread Safe - its little slower
+        // string builder is not thread safe
+
+        // Constructor - 2
     }
 }
