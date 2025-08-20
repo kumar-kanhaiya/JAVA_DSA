a2f6d54dd27eaaff2cac45e525d0bb384e5e45b8
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-20 16:32:25 +0530

    code

--- src/StringBuffer/RandomString.java
+++ src/StringBuffer/RandomString.java
@@ -1,8 +1,25 @@
 package StringBuffer;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
 public class RandomString {
     public static void main(String[] args) {
         System.out.println(generate(5));
+        String sentence = "Hi h   hjh  hjkso  siowi     w ";
+        System.out.println(sentence);
+
+        System.out.println(sentence.replaceAll("\\s", ""));
+
+        // split
+
+        String arr = "Kunal,Apoorv,Rahul,Snehal";
+        String[] names = arr.split(",");
+        System.out.println(Arrays.toString(names));
+
+        // rounding off
+        DecimalFormat df = new DecimalFormat("00.0000");
+        System.out.println(df.format(7.29));
 
     }
 
