a5b95723d34566248da82fe8dbefdb83ab8387bf
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-21 15:31:34 +0530

    code

--- src/FileHandling/Main.java
+++ src/FileHandling/Main.java
@@ -7,20 +7,20 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
 
-//       try(InputStreamReader isr = new InputStreamReader(System.in)){
-//           System.out.println("Enter some letters : ");
-//           int letters =  isr.read();
-//           while(isr.ready()){
-//               System.out.println((char) letters);
-//               letters = isr.read();
-//           }
-////           isr.close(); - no need to close try catch automatically close it
-//           System.out.println();
-//       }
-//       catch(IOException e){
-//           System.out.println(e.getMessage());
-//
-//        }
+       try(InputStreamReader isr = new InputStreamReader(System.in)){
+           System.out.println("Enter some letters : ");
+           int letters =  isr.read();
+           while(isr.ready()){
+               System.out.println((char) letters);
+               letters = isr.read();
+           }
+//           isr.close(); - no need to close try catch automatically close it
+           System.out.println();
+       }
+       catch(IOException e){
+           System.out.println(e.getMessage());
+
+        }
 
        try(FileReader fr = new FileReader("data.txt")){
            System.out.println("Enter some letters : ");
