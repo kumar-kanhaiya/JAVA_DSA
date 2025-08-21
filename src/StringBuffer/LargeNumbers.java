d0df64cd4ca2122aee013f76f9437f9e41a1f765
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-21 13:00:43 +0530

    cdoe

--- src/StringBuffer/LargeNumbers.java
+++ src/StringBuffer/LargeNumbers.java
@@ -67,30 +67,30 @@ public class LargeNumbers {
         int a = 30;
         int b = 67;
 
-        BigInteger B = BigInteger.valueOf(6); // convert int/long to BI
-        int c = B.intValue(); // convert BI to int
-        BigInteger C = new BigInteger("2345678654325678976543256789");
-        BigInteger X = new BigInteger("4536789765432");
-
-        // constants
-        BigInteger D = BigInteger.TEN;
-
-        // operations
-        BigInteger s = C.add(X);
-
-        BigInteger m = C.multiply(X);
-
-        BigInteger sub = C.subtract(X);
-
-        BigInteger d = C.divide(X);
-
-        BigInteger rem = C.remainder(X);
-
-        if (X.compareTo(C) < 0) {
-            System.out.println("Yes");
-        }
-
-
-    }
-
-}
+//        BigInteger B = BigInteger.valueOf(6); // convert int/long to BI
+//        int c = B.intValue(); // convert BI to int
+//        BigInteger C = new BigInteger("2345678654325678976543256789");
+//        BigInteger X = new BigInteger("4536789765432");
+//
+//        // constants
+//        BigInteger D = BigInteger.TEN;
+//
+//        // operations
+//        BigInteger s = C.add(X);
+//
+//        BigInteger m = C.multiply(X);
+//
+//        BigInteger sub = C.subtract(X);
+//
+//        BigInteger d = C.divide(X);
+//
+//        BigInteger rem = C.remainder(X);
+//
+//        if (X.compareTo(C) < 0) {
+//            System.out.println("Yes");
+//        }
+//
+//
+//    }
+//
+//}
