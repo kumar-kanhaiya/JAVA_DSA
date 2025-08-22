2ab4c4c61bef2081f2a7608a3411a14115e0ccd3
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-22 13:40:52 +0530

    code

--- src/BinarySearchInterviewQuestion/BFS/Problem102.java
+++ src/BinarySearchInterviewQuestion/BFS/Problem102.java
@@ -9,21 +9,21 @@ import java.util.List;
 import java.util.Queue;
 
 public class Problem102 {
-    /*
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
-     */
+
+//     * Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+         TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
 
     public static List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
---
+++ src/BinarySearchInterviewQuestion/problem637.java
@@ -0,0 +1,26 @@
+package BinarySearchInterviewQuestion;
+
+import java.util.List;
+
+public class problem637 {
+
+//     * Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    public static List<Double> averageOfLevels(TreeNode root) {
+
+    }
+
+
+}
