cf2a2f8a609d0f64fe11cae93c2964e8fbe0312e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-22 14:05:30 +0530

    code

--- src/BinarySearchInterviewQuestion/problem637.java
+++ src/BinarySearchInterviewQuestion/problem637.java
@@ -1,6 +1,11 @@
 package BinarySearchInterviewQuestion;
 
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class problem637 {
 
@@ -19,7 +24,32 @@ public class problem637 {
       }
 
     public static List<Double> averageOfLevels(TreeNode root) {
+          List<Double> result = new ArrayList<>();
 
+          if(root == null){
+              return result;
+          }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int level = queue.size();
+            double average = 0;
+
+            for (int i = 0; i < level ; i++) {
+                TreeNode currentNode = queue.poll();
+                average += currentNode.val;
+                if(currentNode.left != null){
+                    average += currentNode.left.val;
+                }
+                if(currentNode.right != null){
+                    average += currentNode.right.val;
+                }
+            }
+            result.add(average);
+        }
+
+        return result;
     }
 
 
