0f00b352486a8bec3645d7cee4ff29b8190ed861
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-22 11:58:39 +0530

    code

--- src/BinarySearchInterviewQuestion/BFS/Problem102.java
+++ src/BinarySearchInterviewQuestion/BFS/Problem102.java
@@ -1,6 +1,10 @@
 package BinarySearchInterviewQuestion.BFS;
 
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Problem102 {
     /*
@@ -20,6 +24,16 @@ public class Problem102 {
      */
 
     public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
 
+        if(root == null){
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+
+        return result;
     }
 }
