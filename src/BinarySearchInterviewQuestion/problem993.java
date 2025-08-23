b57a3a4838031583f167b5f46f14128fecfe0616
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-23 14:04:33 +0530

    code

--- src/BinarySearchInterviewQuestion/problem993.java
+++ src/BinarySearchInterviewQuestion/problem993.java
@@ -1,5 +1,8 @@
 package BinarySearchInterviewQuestion;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class problem993 {
 //     * Definition for a binary tree node.
       public class TreeNode {
@@ -16,7 +19,38 @@ public class problem993 {
       }
 
     public boolean isCousins(TreeNode root, int x, int y) {
+        // BFS algo
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean checkX = false;
+        boolean checkY = false;
 
+        while(!queue.isEmpty()){
+            int level = queue.size();
+
+            for (int i = 0; i < level; i++) {
+                TreeNode currentNode = queue.poll();
+                if(currentNode.val == x){
+                    checkX = true;
+                }
+                if(currentNode.val == y){
+                    checkY = true;
+                }
+                if(currentNode.left != null) {
+                    queue.offer(currentNode.left);
+
+                }
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right);
+                }
+
+                if(checkX == true && checkY == true){
+                    return true;
+                }
+
+            }
+        }
+        return false;
     }
 
 }
