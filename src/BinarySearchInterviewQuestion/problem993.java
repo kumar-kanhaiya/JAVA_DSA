58be8d9ff406877cf2639f068fef9cc43df19641
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-23 13:53:04 +0530

    code

---
+++ src/BinarySearchInterviewQuestion/problem993.java
@@ -0,0 +1,22 @@
+package BinarySearchInterviewQuestion;
+
+public class problem993 {
+//     * Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+
+    }
+
+}
