21806492f127c1782824c3003b1af30f1af024ea
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-23 13:48:17 +0530

    code

--- src/BinarySearchInterviewQuestion/problem199.java
+++ src/BinarySearchInterviewQuestion/problem199.java
@@ -1,6 +1,9 @@
 package BinarySearchInterviewQuestion;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class problem199 {
 
@@ -18,6 +21,32 @@ public class problem199 {
           }
       }
     public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+          if(root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+          queue.offer(root);
+          while(!queue.isEmpty()){
+              int level = queue.size();
+              for (int i = 0; i < level; i++) {
+
+
+                  TreeNode currentNode = queue.poll();
+
+                  if (i == level - 1){
+                      result.add(currentNode.val);
+                  }
+
+                      if (currentNode.left != null) {
+                          queue.offer(currentNode.left);
+                      }
+                  if (currentNode.right != null) {
+                      queue.offer(currentNode.right);
+                  }
+              }
+          }
+          return result;
 
     }
 }
