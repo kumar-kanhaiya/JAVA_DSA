c32ee13bb6b973ccef5142f7f835797e03bcc848
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-23 08:52:30 +0530

    cdoe

--- src/BinarySearchInterviewQuestion/Problem103.java
+++ src/BinarySearchInterviewQuestion/Problem103.java
@@ -1,19 +1,24 @@
 package BinarySearchInterviewQuestion;
 
+import java.util.List;
+
 public class Problem103 {
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     *     int val;
-     *     TreeNode left;
-     *     TreeNode right;
-     *     TreeNode() {}
-     *     TreeNode(int val) { this.val = val; }
-     *     TreeNode(int val, TreeNode left, TreeNode right) {
-     *         this.val = val;
-     *         this.left = left;
-     *         this.right = right;
-     *     }
-     * }
-     */
+//     Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+     }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+    }
+
 }
