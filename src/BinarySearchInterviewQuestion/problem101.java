e4b71b471e0902ad43be36cf90c02c729a355220
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-23 18:12:40 +0530

    code

--- src/BinarySearchInterviewQuestion/problem101.java
+++ src/BinarySearchInterviewQuestion/problem101.java
@@ -1,5 +1,8 @@
 package BinarySearchInterviewQuestion;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class problem101 {
 
 // * Definition for a binary tree node.
@@ -17,6 +20,29 @@ public class problem101 {
   }
 
     public boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
 
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean check = true;
+
+        while(!queue.isEmpty()){
+            int level = queue.size();
+            for (int i = 0; i <level ; i++) {
+                TreeNode currentNode = queue.poll();
+                if(currentNode.left != currentNode.right){
+                    check = false;
+                }
+                if(currentNode.left != null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right);
+                }
+            }
+        }
+        return check;
     }
 }
