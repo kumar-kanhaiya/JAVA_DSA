86da2c2532286962e2f8a978e1c99613efc1889a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-24 12:33:13 +0530

    code

--- src/BinarySearchInterviewQuestion/DFS/DFSAlgo.java
+++ src/BinarySearchInterviewQuestion/DFS/DFSAlgo.java
@@ -2,6 +2,17 @@ package BinarySearchInterviewQuestion.DFS;
 
 public class DFSAlgo {
     public static void main(String[] args) {
+        // DFS depth for search
+        // BFS is level by level
+        // DFS is top to bottom
+        // types of BFS
+        // pre , in , post - order
+        // pre - order :-
+        // node -> left -> right
+        // in - order :-
+        // left -> node -> right
+        // post - order :-
+        // left -> Right -> Node
 
     }
 
