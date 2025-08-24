1383907be8db200c78f1208db49d1948159ce8d3
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-24 13:21:43 +0530

    code

--- src/BinarySearchInterviewQuestion/DFS/Problem543.java
+++ src/BinarySearchInterviewQuestion/DFS/Problem543.java
@@ -22,7 +22,7 @@ public class Problem543 {
       int diameter = 0;
     public int diameterOfBinaryTree(TreeNode root){
         height(root);
-        return diameter;
+        return diameter - 1;
     }
 
 
@@ -34,6 +34,8 @@ public class Problem543 {
         int leftHeight = height(node.left);
         int rightHeight = height(node.right);
 
+        int dia = leftHeight  + rightHeight + 1;
+        diameter = Math.max(diameter , dia);
 
         return Math.max(leftHeight,rightHeight) + 1;
     }
