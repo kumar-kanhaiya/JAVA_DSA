faa1331779a8bdef4a1e6071308c2a92f4f3eed0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-24 15:54:11 +0530

    code

--- src/BinarySearchInterviewQuestion/DFS/problem114.java
+++ src/BinarySearchInterviewQuestion/DFS/problem114.java
@@ -18,6 +18,23 @@ public class problem114 {
 
 
     public void flatten(TreeNode root) {
+        if(root == null){
+            return ;
+        }
+        TreeNode current = root;
+        while(current != null){
+            if(current.left != null){
+                TreeNode temp  = current.left;
+                while(temp.right != null){
+                    temp = temp.right;
+                }
+                temp.right = current.right;
+                current.right = current.left;
+                current.left = null;
+            }
+            current = current.right;
+        }
+
 
     }
 }
