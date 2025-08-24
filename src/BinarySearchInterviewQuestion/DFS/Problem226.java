c5cc4a758966d7ddc035350b8dbbe87cff1823cd
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-24 13:59:24 +0530

    codE

--- src/BinarySearchInterviewQuestion/DFS/Problem226.java
+++ src/BinarySearchInterviewQuestion/DFS/Problem226.java
@@ -17,6 +17,14 @@ public class Problem226 {
       }
 
     public TreeNode invertTree(TreeNode root) {
+          if(root == null){
+              return root;
+          }
 
+          TreeNode leftNode = invertTree(root.left);
+          TreeNode rightNode = invertTree(root.right);
+          root.right = leftNode;
+          root.left = rightNode;
+          return root;
     }
 }
