44a0adc78f632cbc400bc3c7db4a223c65c93069
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-24 17:56:17 +0530

    code

--- src/BinarySearchInterviewQuestion/DFS/Problem236.java
+++ src/BinarySearchInterviewQuestion/DFS/Problem236.java
@@ -11,14 +11,24 @@ public class Problem236 {
       }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == p || root == q){
-            return root;
-        }
+
           if(root == null){
             return null;
         }
+        if(root == p || root == q){
+            return root;
+        }
 
+        TreeNode left = lowestCommonAncestor(root.left , p , q);
+        TreeNode right = lowestCommonAncestor(root.right , p , q);
 
+        if(left != null && right != null){
+            return root;
+        }
+        if(left == null){
+            return right;
+        }
+        return left;
     }
 }
 
