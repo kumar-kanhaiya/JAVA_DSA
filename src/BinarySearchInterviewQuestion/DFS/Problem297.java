b527ea68ff8462c65419c3cee875dad81cf04097
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-25 14:14:10 +0530

    codE

--- src/BinarySearchInterviewQuestion/DFS/Problem297.java
+++ src/BinarySearchInterviewQuestion/DFS/Problem297.java
@@ -1,7 +1,6 @@
 package BinarySearchInterviewQuestion.DFS;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Problem297 {
 
@@ -13,7 +12,7 @@ public class Problem297 {
           TreeNode(int x) { val = x; }
       }
 
-      public List<String> serialize(TreeNode node){
+      public List<String> serialize1(TreeNode node){
         List<String> list = new ArrayList<>();
         helper(node,list);
         return list;
@@ -28,13 +27,46 @@ public class Problem297 {
             helper(node.right,strings);
 
       }
+
+      TreeNode deserialize(List<String> list){
+          Collections.reverse(list);
+          TreeNode node = helper2(list);
+          return node;
+      }
+      TreeNode helper3(List<String> list){
+          String val = list.remove(list.size() - 1);
+
+          if(val.charAt(0) == 'n'){
+              return null;
+          }
+          TreeNode node = new TreeNode(Integer.parseInt(val));
+          node.left = helper2(list);
+          node.right = helper2(list);
+          return node;
+      }
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-
+        List<String> list = new ArrayList<>();
+        helper(root, list);
+        return String.join(",", list);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-
+        List<String> list = new ArrayList<>(Arrays.asList(data.split(",")));
+        return helper2(list);
     }
+
+    private TreeNode helper2(List<String> list) {
+        String val = list.remove(0);
+        if (val.equals("null")) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(val));
+        node.left = helper2(list);
+        node.right = helper2(list);
+        return node;
+    }
+
+
 }
