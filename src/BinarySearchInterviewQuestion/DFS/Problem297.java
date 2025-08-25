83263e66189184c8dc40a7306808702402722ea5
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-25 14:02:40 +0530

    code

--- src/BinarySearchInterviewQuestion/DFS/Problem297.java
+++ src/BinarySearchInterviewQuestion/DFS/Problem297.java
@@ -1,5 +1,8 @@
 package BinarySearchInterviewQuestion.DFS;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem297 {
 
 //     * Definition for a binary tree node.
@@ -8,6 +11,22 @@ public class Problem297 {
           TreeNode left;
           TreeNode right;
           TreeNode(int x) { val = x; }
+      }
+
+      public List<String> serialize(TreeNode node){
+        List<String> list = new ArrayList<>();
+        helper(node,list);
+        return list;
+      }
+      void helper(TreeNode node , List<String> strings){
+            if(node == null){
+                strings.add(null);
+                return;
+            }
+            strings.add(String.valueOf(node.val));
+            helper(node.left,strings);
+            helper(node.right,strings);
+
       }
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
