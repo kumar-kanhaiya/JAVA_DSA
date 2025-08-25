f1d378f14ecacd962f31e667ecfa89b2a69cfdc0
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-25 21:47:18 +0530

    code

--- src/BinarySearchInterviewQuestion/DFS/Problem124.java
+++ src/BinarySearchInterviewQuestion/DFS/Problem124.java
@@ -7,7 +7,9 @@ public class Problem124 {
           int val;
           TreeNode left;
           TreeNode right;
-          TreeNode() {}
+          TreeNode() {
+
+          }
           TreeNode(int val) { this.val = val; }
           TreeNode(int val, TreeNode left, TreeNode right) {
               this.val = val;
@@ -15,8 +17,26 @@ public class Problem124 {
               this.right = right;
           }
       }
-
-    public int maxPathSum(TreeNode root) {
-
+    int ans = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode node) {
+        helper(node);
+        return ans;
     }
+    int helper(TreeNode node ){
+        if(node == null){
+            return 0;
+        }
+
+        int left = helper(node.left);
+        int right = helper(node.right);
+
+        left = Math.max(0,left);
+        right = Math.max(0,right);
+
+        int pathSum = left + right + node.val;
+        ans = Math.max(ans, pathSum);
+
+        return Math.max(left,right) + node.val;
+    }
+
 }
