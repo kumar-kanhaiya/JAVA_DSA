6b0fd6fa087aa7d078810772736f1ae57dd3b90f
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-25 13:44:25 +0530

    code

---
+++ src/BinarySearchInterviewQuestion/DFS/Problem297.java
@@ -0,0 +1,13 @@
+package BinarySearchInterviewQuestion.DFS;
+
+public class Problem297 {
+
+//     * Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+      }
+
+}
