64d3ef9723a520d0318dfa08df641898d244d489
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-25 13:44:55 +0530

    codE

--- src/BinarySearchInterviewQuestion/DFS/Problem297.java
+++ src/BinarySearchInterviewQuestion/DFS/Problem297.java
@@ -9,5 +9,13 @@ public class Problem297 {
           TreeNode right;
           TreeNode(int x) { val = x; }
       }
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
 
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+    }
 }
