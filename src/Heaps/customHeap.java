80744e356f188d8ec9504939a90807771dfdc80c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-26 19:17:26 +0530

    code

--- src/Heaps/customHeap.java
+++ src/Heaps/customHeap.java
@@ -61,4 +61,24 @@ public class  customHeap<T extends Comparable<T>> {
         }
         return temp;
     }
+
+    private void downheap(int index){
+        int min = index;
+        int left = left(index);
+        int right = right(index);
+        if(left< list.size() && list.get(min).compareTo(list.get(left)) > 0){
+            min = left;
+        }
+
+        if(right< list.size() && list.get(min).compareTo(list.get(right)) > 0){
+            min = right;
+        }
+
+        if(min!= index){
+            swap(min,index);
+            downheap(min);
+        }
+
+    }
+
 }
