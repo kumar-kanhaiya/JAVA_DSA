7ce2ffa07bec6676bd253153221442171df4b73a
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-26 19:38:57 +0530

    code

--- src/Heaps/Main.java
+++ src/Heaps/Main.java
@@ -1,6 +1,20 @@
 package Heaps;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        customHeap<Integer> heap = new customHeap<>();
 
+        heap.insert(35);
+        heap.insert(15);
+        heap.insert(18);
+        heap.insert(23);
+        heap.insert(42);
+        heap.insert(11);
+        heap.insert(9);
+        heap.insert(88);
+        heap.insert(46);
+
+        System.out.println(heap.remove());
+    }
 
 }
