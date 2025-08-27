6569667d8f5ed9c915fd8149dfed9026ce2e98df
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-27 09:22:19 +0530

    code

--- src/HashMaps/Main.java
+++ src/HashMaps/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
         /*
         HashMap is the key value pairs
+        HashCode :- it is the method of getting a number or integer
 
          */
+        String name = "kanhaiya";
+        int code = name.hashCode();
+        System.out.println(code);
     }
 }
