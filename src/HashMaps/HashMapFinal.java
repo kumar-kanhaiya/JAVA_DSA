97e978c04d4b8236e432bc92c8d6d9489531e88c
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-28 09:31:12 +0530

    code

--- src/HashMaps/HashMapFinal.java
+++ src/HashMaps/HashMapFinal.java
@@ -6,15 +6,67 @@ import java.util.LinkedList;
 
 public class HashMapFinal<K,V> {
         ArrayList<LinkedList<Entity>> list;
-}
 
-private class Entity{
-    String key ;
-    String value;
+        private int size = 0;
+        private float lf = 0.5f;
 
-    public Entity(String key , String value){
-        this.key = key;
-        this.value = value;
+
+        public HashMapFinal(){
+            list = new ArrayList<>();
+            for (int i = 0; i < 10 ; i++) {
+                list.add(new LinkedList<>());
+            }
+        }
+
+        public void put(K key , V value){
+            int hash = Math.abs(key.hashCode()% list.size());
+            LinkedList<Entity> entities = list.get(hash);
+
+            for(Entity entity : entities){
+                if(entity.key.equals(key)){
+                    entity.value = value;
+                    return;
+                }
+
+            }
+            if((float)(size) / list.size() > lf){
+                reHash();
+            }
+            entities.add(new Entity(key , value));
+            size++;
+        }
+
+        private void reHash(){
+            System.out.println("we are now rehashing ");
+            ArrayList<LinkedList<Entity>> old = list;
+            list = new ArrayList<>();
+
+            size = 0;
+            for (int i = 0; i < old.size() * 2; i++) {
+                list.add(new LinkedList<>());
+                
+            }
+            for(LinkedList<Entity> entities : old){
+                for(Entity entry : entities){
+                    put(entry.key , entry.value);
+                }
+            }
+        }
+        private class Entity{
+            String key ;
+            String value;
+
+        public Entity(String key , String value){
+
+            this.key = key;
+            this.value = value;
+        }
+
+
+
     }
 
+
 }
+
+
