1949ee27f8af2dc0602d4003b7740ecba8c4a07e
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-29 12:00:20 +0530

    code

--- src/SortingAlgo/CountSort.java
+++ src/SortingAlgo/CountSort.java
@@ -4,4 +4,22 @@ public class CountSort {
     public static void main(String[] args) {
 
     }
+    public static void countSort(int[] array ){
+        if(array == null || array.length <= 1){
+            return;
+        }
+        int max = maxNumber(array);
+
+
+
+    }
+    public static int maxNumber(int[] array){
+        int max = array[0];
+        for(int num : array){
+            if(num > max){
+                max = num;
+            }
+        }
+        return max;
+    }
 }
