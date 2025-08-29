be6de8142c98e464e8a692f55ad1a7db0edc27b1
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-30 01:11:59 +0530

    codE

--- src/SortingAlgo/RadixSort.java
+++ src/SortingAlgo/RadixSort.java
@@ -4,38 +4,51 @@ import java.util.Arrays;
 
 public class RadixSort {
     public static void main(String[] args) {
+        int[] arr = {29,83,471,37,98,8};
+        System.out.println("Original Array");
+        System.out.println(Arrays.toString(arr));
+        radixSort(arr);
+        System.out.println("After applying radixSort");
+        System.out.println(Arrays.toString(arr));
 
     }
 
-    public static void radixSort(int[] array ){
-        int max = Arrays.stream(array).max().getAsInt();
+    public static void radixSort(int[] arr) {
+        int max = Arrays.stream(arr).max().getAsInt();
 
         // do count sort for every digit place
-        for(int exp = 1; max/exp > 0 ; exp *=10 ){
-            countSort(array , exp);
+        for(int exp = 1; max/exp > 0; exp *= 10) {
+            countSort(arr, exp);
         }
     }
-    private static void countSort(int[] array , int exp){
-        int n = array.length;
+
+    private static void countSort(int[] arr, int exp) {
+        int n = arr.length;
         int[] output = new int[n];
         int[] count = new int[10];
 
-        Arrays.fill(count , 0);
+        Arrays.fill(count, 0);
 
-        for(int i = 0 ; i<n ; i++){
-            count[(array[i]/exp) % 10]++;
+        for(int i=0; i<n; i++) {
+            count[(arr[i] / exp) % 10]++;
         }
 
-        for(int i = 0 ;i<10 ; i++){
+        System.out.println("\nCount array for " + exp + " : " + Arrays.toString(count));
+
+        for(int i=1; i<10; i++) {
             count[i] = count[i] + count[i-1];
         }
 
-        for(int i = n-1 ; i>= 0 ; i--){
-            output[count[(array[i]/exp) % 10] - 1] = array[i];
-            count[(array[i]/exp) % 10]--;
+        System.out.println("Updated count array " + Arrays.toString(count));
 
+        for(int i=n-1; i>=0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
         }
-        System.arraycopy(output , 0 ,array , 0 , n);
+
+        System.out.println("Output array " + Arrays.toString(output));
+
+        System.arraycopy(output, 0, arr, 0, n);
 
     }
 }
