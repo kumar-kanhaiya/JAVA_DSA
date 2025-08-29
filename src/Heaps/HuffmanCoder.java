15f8bac9d9753d7c505161235c99f54d9403cddd
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-30 01:58:35 +0530

    codE

--- src/Heaps/HuffmanCoder.java
+++ src/Heaps/HuffmanCoder.java
@@ -1,7 +1,28 @@
 package Heaps;
 
-public class HuffmanCoder {
-    public static void main(String[] args) {
+import java.util.HashMap;
 
+public class HuffmanCoder {
+    HashMap<Character , String> encoder;
+    HashMap<String , Character> decoder;
+
+    private class Node implements Comparable<Node>{
+        Character data ;
+        int cost; // frequency
+        Node left ;
+        Node right;
+
+        public Node(Character data , int cost){
+            this.data = data;
+            this.cost = cost;
+            this.left = null;
+            this.right = null;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return 0;
+        }
     }
+
 }
