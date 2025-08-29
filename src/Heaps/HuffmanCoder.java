f6c71879eb6abedc4e3d29489b893813bfe42836
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-30 02:05:23 +0530

    code

--- src/Heaps/HuffmanCoder.java
+++ src/Heaps/HuffmanCoder.java
@@ -20,8 +20,23 @@ public class HuffmanCoder {
         }
 
         @Override
-        public int compareTo(Node o) {
-            return 0;
+        public int compareTo(Node other) {
+            return this.cost - other.cost;
+        }
+    }
+    public HuffmanCoder(String feeder) throws Exception{
+        HashMap<Character , Integer> fmap = new HashMap<>();
+
+        for(int i = 0 ; i<feeder.length() ; i++){
+            char cc = feeder.charAt(i);
+            if(fmap.containsKey(cc)){
+                int ov = fmap.get(cc);
+                ov += 1 ;
+                fmap.put(cc , ov);
+            }
+            else{
+                fmap.put(cc , 1);
+            }
         }
     }
 
