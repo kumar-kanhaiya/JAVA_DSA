39537eb349bd973f5f5b8be316d50d35c6ce8fb6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-31 16:13:17 +0530

    code

---
+++ src/AdvanceTreeQuestions/Problem987.java
@@ -0,0 +1,29 @@
+package AdvanceTreeQuestions;
+
+import java.util.List;
+
+public class Problem987 {
+
+//     * Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {
+
+          }
+          TreeNode(int val) {
+              this.val = val;
+          }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+
+    }
+
+
+}
