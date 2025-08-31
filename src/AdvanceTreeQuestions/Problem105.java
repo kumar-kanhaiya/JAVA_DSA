564cd433e6faa0d3bef465489c95c31037b692e8
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-31 14:47:05 +0530

    code

---
+++ src/AdvanceTreeQuestions/Problem105.java
@@ -0,0 +1,27 @@
+package AdvanceTreeQuestions;
+
+public class Problem105 {
+    public static void main(String[] args) {
+
+    }
+
+
+//     * Definition for a binary tree node.
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+
+}
