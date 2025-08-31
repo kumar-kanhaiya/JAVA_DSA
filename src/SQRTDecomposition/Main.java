c3854d97f8b57ee1c1de01f0f2a881a765c10dcf
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-31 13:43:32 +0530

    code

---
+++ src/SQRTDecomposition/Main.java
@@ -0,0 +1,20 @@
+package SQRTDecomposition;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1,3,5,2,7,6,3,1,4,8};
+        int n = array.length;
+
+        // build a blocks array
+        int sqrt = (int)Math.sqrt(n);
+
+        int blocks_id = -1;
+
+        int[] blocks = new int[sqrt + 1];
+
+        for (int i = 0; i < n ; i++) {
+
+        }
+
+    }
+}
