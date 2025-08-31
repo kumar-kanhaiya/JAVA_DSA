2619f1dc867d2a03b269aeec08a40c189eaae174
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-31 15:00:53 +0530

    code

--- src/AdvanceTreeQuestions/Problem105.java
+++ src/AdvanceTreeQuestions/Problem105.java
@@ -1,5 +1,7 @@
 package AdvanceTreeQuestions;
 
+import java.util.HashMap;
+
 public class Problem105 {
     public static void main(String[] args) {
 
@@ -19,9 +21,36 @@ public class Problem105 {
               this.right = right;
           }
       }
+      static int index = 0;
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer , Integer> map = new HashMap<>();
 
+        for (int i = 0; i < inorder.length ; i++) {
+            map.put(inorder[i] , i );
+        }
+
+        return helper(preorder , inorder , 0 , preorder.length - 1 , map);
+    }
+    public TreeNode helper(int[] preorder , int[] inorder , int left , int right ,
+                           HashMap<Integer , Integer>  map){
+        if(left > right){
+            return null;
+        }
+
+        int current = preorder[index];
+        index++;
+
+        TreeNode node = new TreeNode(current);
+
+        if(left == right){
+            return node;
+        }
+
+        node.left = helper(preorder , inorder , left , index - 1 , map);
+        node.right = helper(preorder , inorder , index+ 1 , right , map);
+
+        return node;
     }
 
 }
